cae30b3641418e5fc8b075fcb5953af27fe29777
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-11-18 23:43:00 -0300

    Adicionando Exercicio completo

--- 03_bibliotecas/bibli_06/Respostas/CaioCostaLopes/main.c
+++ 03_bibliotecas/bibli_06/Respostas/CaioCostaLopes/main.c
@@ -2,13 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char unit_upper(char converted) {
+    return converted = converted - ('a' - 'A');
+}
+
 int main() {
     float temperature, result;
     char converted, to_convert;
 
-    scanf("%d", &temperature);
+    scanf("%f %c %c", &temperature, &to_convert, &converted);
 
-    result = convert_temperature(temperature, convert_func(temperature));
+    if ((to_convert == 'c') && (converted == 'k')) {
+        result = convert_temperature(temperature, convert_celsius_to_kelvin);
+    }
+    if ((to_convert == 'c') && (converted == 'f')) {
+        result = convert_temperature(temperature, convert_celsius_to_fahrenheit);
+    }
+    if ((to_convert == 'k') && (converted == 'c')) {
+        result = convert_temperature(temperature, convert_kelvin_to_celsius);
+    }
+    if ((to_convert == 'k') && (converted == 'f')) {
+        result = convert_temperature(temperature, convert_kelvin_to_fahrenheit);
+    }
+    if ((to_convert == 'f') && (converted == 'c')) {
+        result = convert_temperature(temperature, convert_fahrenheit_to_celsius);
+    }
+    if ((to_convert == 'f') && (converted == 'k')) {
+        result = convert_temperature(temperature, convert_fahrenheit_to_kelvin);
+    }
+    converted = unit_upper(converted);
+
+    printf("Temperature: %.2f%c", result, converted);
+
+    if ((converted == 'C') || (converted == 'F')) {
+        printf("º");
+    }
 
     return 0;
 }
--- 03_bibliotecas/bibli_06/Respostas/CaioCostaLopes/temperature_conversor.c
+++ 03_bibliotecas/bibli_06/Respostas/CaioCostaLopes/temperature_conversor.c
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 
 float convert_celsius_to_kelvin(float temperature) {
-    return temperature + 273.0;
+    return temperature + 273.15;
 }
 
-float convert_celsius_fahrenheit(float temperature) {
-    return (temperature * 1.8) + 32.0;
+float convert_celsius_to_fahrenheit(float temperature) {
+    return (temperature * 1.80) + 32.00;
 }
 
 float convert_kelvin_to_celsius(float temperature) {
-    return temperature - 273.0;
+    return temperature - 273.15;
 }
 
 float convert_kelvin_to_fahrenheit(float temperature) {
-    int c;
+    float c;
 
     c = convert_kelvin_to_celsius(temperature);
 
@@ -23,40 +23,15 @@ float convert_kelvin_to_fahrenheit(float temperature) {
 }
 
 float convert_fahrenheit_to_celsius(float temperature) {
-    return (temperature - 32.0) / 1.8;
+    return (temperature - 32.00) / 1.80;
 }
 
 float convert_fahrenheit_to_kelvin(float temperature) {
-    int c;
+    float c;
 
     c = convert_fahrenheit_to_celsius(temperature);
 
     return convert_celsius_to_kelvin(c);
-}
-
-float convert_func(temperature) {
-    char to_convert, converted;
-    
-    scanf("%c %c\n", &to_convert, &converted);
-
-    if ((to_convert == 'c') && (converted == 'k')) {
-        return convert_celsius_to_kelvin(temperature);
-    }
-    if ((to_convert == 'c') && (converted == 'f')) {
-        return convert_celsius_to_fahrenheit(temperature);
-    }
-    if ((to_convert == 'k') && (converted == 'c')) {
-        return convert_kelvin_to_celsius(temperature);
-    }
-    if ((to_convert == 'k') && (converted == 'f')) {
-        return convert_kelvin_to_fahrenheit(temperature);
-    }
-    if ((to_convert == 'f') && (converted == 'c')) {
-        return convert_fahrenheit_to_celsius(temperature);
-    }
-    if ((to_convert == 'f') && (converted == 'k')) {
-        return convert_fahrenheit_to_kelvin(temperature);
-    }
 }
 
 float convert_temperature(float temperature, float convert_func(float temperature)) {
