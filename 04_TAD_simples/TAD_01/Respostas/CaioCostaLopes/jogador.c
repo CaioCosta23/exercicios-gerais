57e7d8833dea16b8148680389a085a1bac15daf5
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-11-23 23:14:03 -0300

    Retirando bibliotecas desnecessarias (Precisa concertar o 'VenceuJogador()'

---
+++ 04_TAD_simples/TAD_01/Respostas/CaioCostaLopes/jogador.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "jogador.h"
+#include "jogada.h"
+#include "tabuleiro.h"
+
+tJogador CriaJogador(int id_Jogador) {
+    tJogador jogador;
+
+    jogador.id = id_Jogador;
+
+    return jogador;
+}
+
+tTabuleiro JogaJogador(tJogador jogador, tTabuleiro tabuleiro) {
+    tJogada jogada;
+    int x, y;
+
+    while (1) {
+        printf("Jogador %d\n", jogador.id);
+        printf("Digite uma posicao (x e y):\n");
+
+        jogada = LeJogada();
+
+        x = ObtemJogadaX(jogada);
+        y = ObtemJogadaY(jogada);
+        
+        if (!(EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, x, y, jogador.id))) {
+            if (EstaLivrePosicaoTabuleiro(tabuleiro, x, y)) {
+                if (FoiJogadaBemSucedida(jogada)) {
+                    jogada.sucesso = 1;
+                    tabuleiro = MarcaPosicaoTabuleiro(tabuleiro, jogador.id, x, y);
+                    ImprimeTabuleiro(tabuleiro);
+                    break;
+                }else {
+                    printf("Posicao invalida (FORA DO TABULEIRO - [%d, %d]!)\n", jogada.x, jogada.y);
+                }
+            }else {
+                printf("Posicao invalida (OCUPADA - [%d, %d]!)\n", jogada.x, jogada.y);
+            }
+        }else {
+            printf("Posicao invalida (OCUPADA - [%d, %d]!)\n", jogada.x, jogada.y);
+        }
+    }
+    return tabuleiro;
+}
+
+int VenceuJogador(tJogador jogador, tTabuleiro tabuleiro) {
+    int l, c, d;
+    int horizontal, vertical, diagonal;
+
+    for (l = 0; l < TAM_TABULEIRO; l++) {
+        horizontal = 0;
+        vertical = 0;
+        for (c = 0; c < TAM_TABULEIRO; c++) {
+            if (tabuleiro.posicoes[l][c] == jogador.id) {
+                horizontal += 1;
+            }
+            if (tabuleiro.posicoes[c][l] == jogador.id) {
+                vertical += 1;
+            }
+        }
+        if ((horizontal == 3) || (vertical == 3)) {
+            return 1;
+        }
+    }
+
+    diagonal = 0;
+
+    for (d = 0; d < TAM_TABULEIRO; d++) {
+        if (tabuleiro.posicoes[d][d] == jogador.id) {
+            diagonal += 1;
+        }
+    }
+    if (diagonal == 3) {
+        return 1;
+    }
+    return 0;
+}
