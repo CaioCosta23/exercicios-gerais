4475982620ccd4b7c48f668bbfd91e5ec536e422
CaioCosta23 <caio.c.lopes@ufes.edu.br>
2023-11-27 16:13:24 -0300

    Adicionando versao atualizada do jogador (ainda incompleta - erro no 'VenceuJogador()'

--- 04_TAD_simples/TAD_01/Respostas/CaioCostaLopes/jogador.c
+++ 04_TAD_simples/TAD_01/Respostas/CaioCostaLopes/jogador.c
@@ -42,17 +42,17 @@ tTabuleiro JogaJogador(tJogador jogador, tTabuleiro tabuleiro)
                     }
                     else
                     {
-                        printf("Posicao invalida (OCUPADA - [%d, %d]!)\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
+                        printf("Posicao invalida (OCUPADA - [%d, %d])!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
                     }
                 }
                 else
                 {
-                    printf("Posicao invalida (OCUPADA - [%d, %d]!)\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
+                    printf("Posicao invalida (OCUPADA - [%d, %d])!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
                 }
             }
             else
             {
-                printf("Posicao invalida (FORA DO TABULEIRO - [%d, %d]!)\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
+                printf("Posicao invalida (FORA DO TABULEIRO - [%d, %d])!\n", ObtemJogadaX(jogada), ObtemJogadaY(jogada));
             }
         }
     }
@@ -67,45 +67,30 @@ int VenceuJogador(tJogador jogador, tTabuleiro tabuleiro)
     for (l = 0; l < TAM_TABULEIRO; l++)
     {
         horizontal = 0;
+        vertical = 0;
+        primeDiagonal = 0;
         for (c = 0; c < TAM_TABULEIRO; c++)
         {
             if (tabuleiro.posicoes[l][c] == jogador.id)
             {
                 horizontal += 1;
             }
-        }
-        if (horizontal == 3)
-        {
-            return 1;
-        }
-    }
-
-    for (l = 0; l < TAM_TABULEIRO; l++)
-    {
-        vertical = 0;
-        for (c = 0; c < TAM_TABULEIRO; c++)
-        {
             if (tabuleiro.posicoes[c][l] == jogador.id)
             {
                 vertical += 1;
             }
+            // Diagonal onde os elementos que formam a diagonal tem tanto o índice da linha quanto da coluna tendo o mesmo valor;
+            if ((tabuleiro.posicoes[c][c] == jogador.id))
+            {
+                primeDiagonal += 1;
+            }
         }
-        if (vertical == 3)
+        if ((horizontal == 3) || (vertical) == 3)
         {
             return 1;
         }
     }
 
-    primeDiagonal = 0;
-
-    for (d = 0; d < TAM_TABULEIRO; d++)
-    {
-        // Diagonal onde os elementos que formam a diagonal tem tanto o índice da linha quanto da coluna tendo o mesmo valor;
-        if ((tabuleiro.posicoes[d][d] == jogador.id))
-        {
-            primeDiagonal += 1;
-        }
-    }
     // Verifica se o jogador venceu na diagonal "secundária" (diagonal onde os indíces da linha e da coluna não tem o mesmo valor);
     if ((tabuleiro.posicoes[0][TAM_TABULEIRO - 1] == jogador.id) && (tabuleiro.posicoes[TAM_TABULEIRO / 2][TAM_TABULEIRO / 2] == jogador.id) && (tabuleiro.posicoes[TAM_TABULEIRO - 1][0] == jogador.id))
     {
