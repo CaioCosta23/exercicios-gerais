f14a20a805bd68b5c9ca516a81b8facca4e61604
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-02 19:25:03 -0300

    Adicionando funcoes da biblioteca do candidato

---
+++ 04_TAD_simples/TAD_02/Respostas/CaioCostaLopes/candidato.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "candidato.h"
+
+#define TAM_MAX 50
+
+tCandidato CriarCandidato(char nome[], char partido[], char cargo, int id) {
+    tCandidato candidato;
+
+    strcpy(candidato.nome, nome);
+    strcpy(candidato.partido, partido);
+    candidato.cargo = cargo;
+    candidato.id = id;
+    candidato.votos = 0;
+
+    return candidato;
+}
+
+tCandidato LeCandidato() {
+    char nome[TAM_MAX], partido[TAM_MAX];
+    char cargo;
+    int id;
+    tCandidato candidato;
+
+    scanf("%[^,], %[^,], %c, %d\n", nome, partido, &cargo, &id);
+    
+    candidato = CriarCandidato(nome, partido, cargo, id);
+
+    return candidato;
+}
+
+int VerificaIdCandidato(tCandidato candidato, int id) {
+    return (candidato.id == id);
+}
+
+int EhMesmoCandidato(tCandidato candidato1, tCandidato candidato2) {
+    if ((candidato1.id == candidato2.id) && (strcmp(candidato1.partido, candidato2.partido) == 0)) {
+        return 1;
+    }
+    return 0;
+}
+
+char ObtemCargo(tCandidato candidato) {
+    return candidato.cargo;
+}
+
+tCandidato IncrementaVotoCandidato(tCandidato candidato) {
+    candidato.votos += 1;
+
+    return candidato;
+}
+
+int ObtemVotos(tCandidato candidato) {
+    return candidato.votos;
+}
+
+float CalculaPercentualVotos(tCandidato candidato, int totalVotos) {
+    float percentual = 0.00;
+
+    percentual = (candidato.votos * 100) / totalVotos;
+
+    return percentual;
+}
+
+void ImprimeCandidato(tCandidato candidato, float percentualVotos) {
+    printf("%s (%s), %d voto(s), %.1f%%", candidato.nome, candidato.partido, candidato.votos, percentualVotos);
+}
