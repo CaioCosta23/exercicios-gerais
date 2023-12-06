9608f466fa3dc044ae90254845240e16f6a51fe2
CaioCosta23 <caio.c.lopes@ufes.edu.br>
2023-12-06 13:01:26 -0300

    Adicionado funcoes da biblioteca do aluno

---
+++ 04_TAD_simples/TAD_04/Respostas/CaioCostaLopes/aluno.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "aluno.h"
+
+#define MAX_CARACTERES 50
+
+tAluno CriaAluno(char nome[], int matricula, int n1, int n2, int n3) {
+    tAluno aluno;
+
+    strcpy(aluno.nome, nome);
+    aluno.matricula = matricula;
+    aluno.n1 = n1;
+    aluno.n2 = n2;
+    aluno.n3 = n3;
+
+    return aluno;
+}
+
+tAluno LeAluno() {
+    char nome[MAX_CARACTERES];
+    int matricula, n1, n2, n3;
+    tAluno aluno;
+
+    scanf("%49[^\n]\n", nome);
+    scanf("%d\n", &matricula);
+    scanf("%d %d %d\n", &n1, &n2, &n3);
+
+    aluno = CriaAluno(nome, matricula, n1, n2, n3);
+
+    return aluno;
+
+}
+
+int ComparaMatricula(tAluno aluno1, tAluno aluno2) {
+    if (aluno1.matricula > aluno2.matricula) {
+        return 1;
+    }else {
+        if (aluno1.matricula < aluno2.matricula) {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+int CalculaMediaAluno(tAluno aluno) {
+    return ((aluno.n1 + aluno.n2 + aluno.n3) / 3);
+}
+
+int VerificaAprovacao(tAluno aluno) {
+    int media = 0;
+
+    media = CalculaMediaAluno(aluno);
+
+    return (media >= 7);
+}
+
+void ImprimeAluno(tAluno aluno) {
+    printf("%s\n", aluno.nome);
+}
