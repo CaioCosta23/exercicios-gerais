92e452aafffa6f9c5541dfe10bb1aa7566b1a109
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-08 11:14:28 -0300

    Adicionando arquivo main corrigido e completo

--- 04_TAD_simples/TAD_05/Respostas/CaioCostaLopes/main.c
+++ 04_TAD_simples/TAD_05/Respostas/CaioCostaLopes/main.c
@@ -7,46 +7,60 @@
 #define TAM_CPF 15
 
 int main () {
-    int qtdContas, op, numero, r;
+    int qtdContas, op, numero, r, v;
     int c = 0;
     float valor;
     char nome[TAM_NOME], cpf[TAM_CPF];
-    tUsuario usuario;
 
-    scanf("%d\n", &qtdContas);
+    scanf("%d", &qtdContas);
 
+    tUsuario usuario[qtdContas];
     tConta conta[qtdContas];
 
     while(1) {
         valor = 0.00;
 
-        scanf("%d\n", &op);
+        scanf("%d", &op);
 
-        switch (op) {
-            case 1:
-                scanf("%f\n", &valor);
-                conta[c] = SaqueConta(conta[c], valor);
-                break;
-            case 2:
-                scanf("%f\n", &valor);
-                conta[c] = SaqueConta(conta[c], valor);
-                break;
-            case 3:
-                scanf("%s %s %d", nome, cpf, &numero);
-                usuario = CriaUsuario(nome, cpf);
-                conta[c] = CriaConta(numero, usuario);
-                c++;
-                break;
-            case 4:
-                if (c < 5) {
-                    for (r = 0; r < qtdContas; r++) {
+        if (op == 0) {
+            break;
+        }else {
+
+            switch (op) {
+                case 1:
+                    scanf("%d%f", &numero, &valor);
+                    for (v = 0; v < c; v ++) {
+                        if (VerificaConta(conta[v], numero)) {
+                            conta[v] = SaqueConta(conta[v], valor);
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    scanf("%d%f", &numero, &valor);
+                    for (v = 0; v < c; v ++) {
+                        if (VerificaConta(conta[v], numero)) {
+                            conta[v] = DepositoConta(conta[v], valor);
+                            break;
+                        }
+                    }
+                    break;
+                case 3:
+                    if (c <= 5) {
+                        scanf("%s %s %d", nome, cpf, &numero);
+                        usuario[c] = CriaUsuario(nome, cpf);
+                        conta[c] = CriaConta(numero, usuario[c]);
+                        c++;
+                    }
+                    break;
+                case 4:
+                    printf("===| Imprimindo Relatorio |===\n");
+                    for (r = 0; r < c; r++) {
                         ImprimeConta(conta[r]);
                         printf("\n");
                     }
-                }
-                break;
-            default:
-                exit(1);
+                    break;
+            }
         }
     }
 
