e0b3664ed2a2a6fad1419693f4196b34a4d6b3ab
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-09 00:27:11 -0300

    Adicionando funcao de multiplicacao, alterando impressao, e modificando variaveis (resultado)

--- 04_TAD_simples/TAD_06/Respostas/CaioCostaLopes/matrix_utils.c
+++ 04_TAD_simples/TAD_06/Respostas/CaioCostaLopes/matrix_utils.c
@@ -27,20 +27,18 @@ void MatrixPrint(tMatrix matrix) {
     int r, c;
 
     for (r = 0; r < matrix.rows; r++) {
+        // Imprime cada barra que tem na borda da matriz (antes do primeiro elemento de cada linha);
+        printf("|");
         for (c = 0; c < matrix.cols; c++) {
-            // Imprime cada barra que tem na borda da matriz (antes do primeiro elemento de cada linha);
-            if (c == 0) {
-                printf("|");
-            }
             // Imprime os elementos da matriz;
             printf("%d", matrix.data[r][c]);
             // Imprime os espaços entre cada elemento da matriz;
-            if (c < (matrix.cols - 2)) {
+            if (c < (matrix.cols - 1)) {
                 printf(" ");
             }
             // Imprime cada barra que tem na borda da matriz (depoiss do último elemento de cada linha);
             if (c == (matrix.cols - 1)) {
-                printf("|");
+                printf("|\n");
             }
         }
         printf("\n");
@@ -61,34 +59,68 @@ int PossibleMatrixMultiply(tMatrix matrix1, tMatrix matrix2) {
 
 tMatrix MatrixAdd(tMatrix matrix1, tMatrix matrix2) {
     int r, c;
-    tMatrix resultado;
+    tMatrix result;
 
     for (r = 0; r < matrix2.rows; r++) {
         for (c = 0; c < matrix2.cols; c++) {
-            resultado.data[r][c] = matrix1.data[r][c] + matrix2.data[r][c];            
+            result.data[r][c] = matrix1.data[r][c] + matrix2.data[r][c];            
         }
     }
-    return resultado;
+    return result;
 }
 
 tMatrix MatrixSub(tMatrix matrix1, tMatrix matrix2) {
     int r, c;
-    tMatrix resultado;
+    tMatrix result;
 
     for (r = 0; r < matrix1.rows; r++) {
         for (c = 0; c < matrix1.cols; c++) {
-            resultado.data[r][c] = matrix1.data[r][c] - matrix2.data[r][c];            
+            result.data[r][c] = matrix1.data[r][c] - matrix2.data[r][c];            
         }
     }
-    return resultado;
+    return result;
 }
 
 tMatrix MatrixMultiply(tMatrix matrix1, tMatrix matrix2) {
-    tMatrix resultado;
-    /*
-    * AINDA SEM CÓDIGO!!!!
-    */
-    return resultado;
+    tMatrix result;
+    
+    int r1, c1, c2, mult;
+
+    r1 = 0;
+
+    // Define o numero de linhas que a matriz resultante tera;
+    while(r1 < matrix1.rows) {
+        c2 = 0;
+
+        // Define o numero de colunas que a matriz resultante tera;
+        while(c2 < matrix2.cols) {
+            mult = 0;
+            c1 = 0;
+
+            // O numero de colunas da primeira matriz e sempre o numero de linhas da segunda, podemos usar qualquer uma como base;
+            while(c1 < matrix1.cols) {
+                /**
+                 * @brief Recebe a soma das multiplicacoes que formarao um numero na matriz resultante;
+                 * 
+                 * Cada elemento "X" de uma linha da primeira mariz e multiplicado por um elemento coluna "Y" da segunda matrix,
+                 * e o resultado dessas multiplicacoes e acumulado, para que surja o elemento N (resultado da multiplicacao) na
+                 * matriz resultante;
+                 */
+                mult += matrix1.data[r1][c1] * matrix2.data[c1][c2];
+                c1++;
+            }
+            /**
+             * @brief Recebe o valor resultado da multiplicacao dos elementos das duas matrizes analisadas e o coloca na posicao
+             * correta da matriz resultante;
+             */
+            result.data[r1][c2] = mult;
+
+            c2++;
+        }
+        r1++;
+    }
+
+    return result;
 }
 
 tMatrix TransposteMatrix(tMatrix matrix) {
