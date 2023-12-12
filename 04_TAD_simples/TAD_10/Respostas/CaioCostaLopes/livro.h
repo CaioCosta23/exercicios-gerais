6d39c7ef04c2b11911bc321b08973b6e8f3cf010
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-12 09:44:16 -0300

    Adicionando biblioteca do livro

---
+++ 04_TAD_simples/TAD_10/Respostas/CaioCostaLopes/livro.h
@@ -0,0 +1,24 @@
+#ifndef LIVRO_H
+#define LIVRO_H
+
+typedef struct {
+    char titulo[100];
+    char autor[50];
+    int anoPublicacao;
+}tLivros;
+
+/**
+ * @brief Lê os atributos de um livro à partir dea entrada padrão;
+ * 
+ * @return tLivros Livro com os seus respectivos atributos lidos;
+ */
+tLivros lerLivros();
+
+/**
+ * @brief Imprime os atributos de um livro;
+ * 
+ * @param livro Livro cujo os atributos erão lidos;
+ */
+void imprimeLivro(tLivros livro);
+
+#endif
