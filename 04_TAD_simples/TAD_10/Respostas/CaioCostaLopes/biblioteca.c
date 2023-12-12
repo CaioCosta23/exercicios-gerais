45d7061e3b2b245e9d3d329221e01dcb5dc4ac42
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-12 10:41:22 -0300

    Adicionando funcoes da biblioteca de biblioetca (de livros)

---
+++ 04_TAD_simples/TAD_10/Respostas/CaioCostaLopes/biblioteca.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "biblioteca.h"
+#include "livro.h"
+
+tBiblioteca inicializaBiblioteca() {
+    tBiblioteca biblioteca;
+
+    biblioteca.tamanho = 0;
+
+    return biblioteca;
+}
+
+tBiblioteca adicionarLivroNaBiblioteca(tBiblioteca biblioteca, tLivros livro) {
+    tBiblioteca biblioteca;
+    
+    if (biblioteca.tamanho < MAX_LIVROS) {
+        biblioteca.livros[biblioteca.tamanho] = livro;
+        biblioteca.tamanho += 1;
+        printf("Livro adicionado com sucesso!\n");
+    }else {
+        printf("Nao eh posiivel adicionar mais livros!\n");
+    }
+    return biblioteca;
+}
+
+tBiblioteca removerLivroDaBiblioteca(tBiblioteca biblioteca, char titulo[]) {
+    int l, o, posicao, a;
+    int achado = 0;
+    tLivros aux; 
+    tLivros livroVazio = {"\0", "\0", 0};;
+
+    if (biblioteca.tamanho > 0) {
+        for (l = 0; l < biblioteca.tamanho; l++) {
+            if (!(verificaTituloDoLivroNaBiblioteca(biblioteca.livros[l], titulo))) {
+                achado = 1;
+                posicao = l;
+                break;
+            }
+        }
+
+        if (achado) {
+            for (o = posicao; o < biblioteca.tamanho - 1; o++) {
+                aux = biblioteca.livros[o + 1];
+                biblioteca.livros[o + 1] = biblioteca.livros[o];
+                biblioteca.livros[o] = aux;
+            }
+            biblioteca.livros[o] = livroVazio;
+            biblioteca.tamanho -= 1;
+            printf("Livro removido com sucesso!\n");
+        }else {
+            printf("Livro nao encontrado na biblioteca!\n");
+        }
+    }
+    return biblioteca;
+}
+
+int verificaTituloDoLivroNaBiblioteca(tLivros livro, char titulo[]) {
+    return strcmp(livro.titulo, titulo);
+}
+
+void listarLivrosDaBiblioteca(tBiblioteca biblioteca) {
+    int l;
+
+    for (l = 0; l < biblioteca.tamanho; l++) {
+        printf("Lista de livros na biblioteca:\n");
+        imprimeLivro(biblioteca.livros[l]);
+    }
+}
