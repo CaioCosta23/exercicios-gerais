9ac45753e35dc9e4c6efcbb36e2a7a139fbe0844
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-12 10:12:03 -0300

    Adicionando funcoes da bibioteca de livros

---
+++ 04_TAD_simples/TAD_10/Respostas/CaioCostaLopes/livro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "livro.h"
+
+tLivros lerLivro() {
+    tLivros livro;
+
+    scanf("%s %s %d", livro.titulo, livro.autor, livro.anoPublicacao);
+
+    return livro;
+}
+
+void imprimeLivro(tLivros livro) {
+    printf("Titulo: %s\n", livro.titulo);
+    printf("Autor: %s\n", livro.autor);
+    printf("Ano de Publicacao: %d\n", livro.anoPublicacao);
+}
