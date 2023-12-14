6a1c90789525140c4bdbb655f32ca7d4313dc086
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-14 19:58:38 -0300

    Adicionando biblioteca

---
+++ 05_ponteiros/pont_01/Respostas/CaioCostaLopes/utils.h
@@ -0,0 +1,14 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+/**
+ * @brief Troca os valores de duas variáveis inteiras. Essa função recebe como parâmetro dois ponteiros para inteiros
+ *  e troca os valores armazenados nas variáveis apontadas por eles;
+ * 
+ * @param n1 Ponteiro para a primeira variável a ser trocada;
+ * @param n2 Ponteiro para a segunda variável a ser trocada;
+ * @return int 1 se a troca foi realizada com sucesso e 0 caso contrário;
+ */
+int TrocaInteiros(int * n1, int * n2);
+
+#endif
