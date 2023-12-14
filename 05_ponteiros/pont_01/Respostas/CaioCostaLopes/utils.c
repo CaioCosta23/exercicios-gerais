0f31da0e9ecfd991554b530e44444b07e50f8db8
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-14 20:40:05 -0300

    Adicionando funcao da bibioteca utils

---
+++ 05_ponteiros/pont_01/Respostas/CaioCostaLopes/utils.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int TrocaInteiros(int *n1, int *n2) {
+    int aux;
+
+    if (*n1 >= *n2) {
+        aux = *n1;
+        *n1 = *n2;
+        *n2 = aux;
+
+        return 1;
+    }
+    return 0;
+}
