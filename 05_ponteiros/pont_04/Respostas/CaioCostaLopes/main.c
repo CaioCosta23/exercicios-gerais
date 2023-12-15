5b70d6f25cd2336da4f1c67730bdce31b0660e9b
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-15 13:51:38 -0300

    Adicionando arquivo principal

---
+++ 05_ponteiros/pont_04/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int main() {
+    int tamanho, maior, menor;
+    float media;
+
+    scanf("%d", &tamanho);
+
+    int numeros[tamanho];
+
+    LeNumeros(numeros, tamanho);
+    EncontraMaiorMenorMedia(numeros, tamanho, &maior, &menor, &media);
+
+    printf("Maior elemento:%d\n", maior);
+    printf("Menor elemento: %d\n", menor);
+    printf("Media dos elementos: %.2f\n", media);
+
+    return 0;
+}
