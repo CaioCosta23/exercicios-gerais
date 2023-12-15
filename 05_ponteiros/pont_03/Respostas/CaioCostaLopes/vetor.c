1cf77d4df219a04425d02f6e470915b87d4a19e7
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-15 13:14:50 -0300

    Adicionando funcoes da biblioteca de vetores

---
+++ 05_ponteiros/pont_03/Respostas/CaioCostaLopes/vetor.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vetor.h"
+
+void LeDadosParaVetor(int *vet, int tam) {
+    int i;
+    int lista[tam];
+
+    for (i = 0; i < tam; i++) {
+        scanf("%d", &lista[i]);
+    }
+    vet = lista;
+}
+
+void ImprimeDadosVetor(int *n, int tam) {
+    int i;
+    
+    for (i = 0; i < tam; i++) {
+        printf("%d ", *(n + i));
+    }
+    printf("\n");
+}
+
+void TrocaSeAchaMenor(int *vet, int tam, int *paraTrocar) {
+    int *x;
+    int aux;
+
+    for (*x = paraTrocar + 1; x < vet + tam; x++) {
+        if (*x < *paraTrocar) {
+            aux = *x;
+            *x = paraTrocar;
+            *paraTrocar = aux;
+        }
+    }
+}
+
+void OrdenaCresecnte(int *vet, int tam) {
+    int i;
+
+    for (i = 0; i < tam; i++) {
+        TrocaSeAchaMenor(vet, tam, vet + i);
+    }
+}
