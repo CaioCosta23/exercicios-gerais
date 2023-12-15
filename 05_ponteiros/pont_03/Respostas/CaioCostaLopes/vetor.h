7b1ea8a5d1ea58046b1ddb9f09abb8a37016dee3
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-15 11:27:50 -0300

    Adicionando biblioteca vetor

---
+++ 05_ponteiros/pont_03/Respostas/CaioCostaLopes/vetor.h
@@ -0,0 +1,39 @@
+#ifndef _VETOR_H_
+#define _VETOR_H_
+
+/**
+ * @brief Lê dados do usuário e armazena em um vetor;
+ * 
+ * @param vet Ponteiro para o vetor que receberá os valores lidos;
+ * @param tam Tamanho do vetor;
+ */
+void LeDadosParaVetor(int *vet, int tam);
+
+/**
+ * @brief Imprime os dados de um vetor na tela;
+ * 
+ * @param n Ponteiro para o vetor a ser impresso;
+ * @param tam Tamanho do vetor;
+ */
+void ImprimeDadosVetor(int *n, int tam);
+
+/**
+ * @brief Troca o valor de duas variáveis se o segundo for menor que o primeiro;
+ * 
+ * @param vet Ponteiro para o vetor a ser percorrido;
+ * @param tam Tamanho do vetor;
+ * @param paraTrocar Ponteiro para a variável que armazenará o indice do menor valor encontrado;
+ * OBS: Essa função tem o comportamento de encontrar o menor valor do vetor de tamanho "tam". Se esse valor for menor que o valor apontado por "paraTrocar",
+ * é realizada a troca de posição. Ao final da execução, a variável apontada por "paraTrocar" terá o menor vaor encontrado;
+ */
+void TrocaSeAchaMenor(int *vet, int tam, int *paraTrocar);
+
+/**
+ * @brief Ordena um vetor em ordem crescente;
+ * 
+ * @param vet Ponteiro para o vetor a ser ordenado;
+ * @param tam Tamanho do vetor;
+ */
+void OrdenaCrescente(int *vet, int tam);
+
+#endif
