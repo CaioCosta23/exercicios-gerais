581406459c13fe93bf4ac22b59eee8fc7b0f30ed
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-15 13:47:41 -0300

    Adicionando biblioteca utils.h

---
+++ 05_ponteiros/pont_04/Respostas/CaioCostaLopes/utils.h
@@ -0,0 +1,23 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+/**
+ * @brief Lê uma sequência de inteiros da entrada padrão e os armazena em um array (lista);
+ * 
+ * @param array Ponteiro para onde os inteiros serao armazenados;
+ * @param tamanho Tamanho do array (lista);
+ */
+void LeNumeros(int *array, int tamanho);
+
+/**
+ * @brief Encontra o maior, o menor e a média dos valores em um array de inteiros;
+ * 
+ * @param array Ponteiro para array de inteiros;
+ * @param tamanho Tamanho do array (lista);
+ * @param maior Ponteiro para uma variável que armazenará o maior valor;
+ * @param menor Ponteiro para uma variável que armazenará o menor valor;
+ * @param media Ponteiro para uma variável que armazenará a média dos valores;
+ */
+void EncontraMaiorMedia(int *array, int tamanho, int *maior, int *menor, int *media);
+
+#endif
