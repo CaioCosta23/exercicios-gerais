8cc2b082c9c31de2b193bf0a4284065289812cc0
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-15 14:03:32 -0300

    Adiciinando funcoes da biblioteca utils

---
+++ 05_ponteiros/pont_04/Respostas/CaioCostaLopes/utils.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+void LeNumeros(int *array, int tamanho) {
+    int i;
+
+    for (i  = 0; i < tamanho; i++) {
+        scanf("%d", array + i);
+    }
+}
+
+void EncontraMaiorMedia(int *array, int tamanho, int *maior, int *menor, float *media) {
+    int i;
+    int soma = 0;
+    
+    for(i = 0; i < tamanho; i++) {
+        if ((i == 0) || (*(array + i) > *maior)) {
+            *maior = *(array + i);
+        }
+        if ((i == 0) || (*(array + i) < *menor)) {
+            *menor = *(array + i);
+        }
+        soma += *(array + i);
+    }
+
+    *media = (float)soma / (float)tamanho;
+}
