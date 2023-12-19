49ff14cf91d2b523e3e0d9a38d91a54848919cea
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-19 12:11:28 -0300

    Adicionando biblioteca de calculadora

---
+++ 05_ponteiros/pont_11/Respostas/CaioCostaLopes/calculadora.h
@@ -0,0 +1,20 @@
+#ifndef _CALCULADORA_H
+#define _CALCULADORA_H
+
+/**
+ * @brief Ponteiro para função que recebe dois números de ponto flutuante (deciamais - 'float') e realiza uma operação matemática;
+ * 
+ */
+typedef float(*CalculadoraCallback)(float, float);
+
+/**
+ * @brief Função que realiza uma operação matemática entre dois números;
+ * 
+ * @param num1 Primeiro Número;
+ * @param num2 Segundo Número;
+ * @param operacao Ponteiro para a função que realiza a operação matemática;
+ * @return float Resultado da operação matemática;
+ */
+float Calcular(float num1, float num2, CalculadoraCallback operacao);
+
+#endif
