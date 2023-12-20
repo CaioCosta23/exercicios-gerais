8d75c74530e4a52bbb695332e2fd5610805daa9c
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-20 11:43:53 -0300

    Corrigindo comentarios

---
+++ 06_alocacao_dinamica/01_geral/aloc_01/Respostas/CaioCostaLopes/utils.h
@@ -0,0 +1,37 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+/**
+ * @brief Cria um vetor de inteiros com o tamanho especificado (alocado dinanamicamente);
+ * 
+ * @param tamanho Tamanho do vetor a ser criado;
+ * @return int Ponteiro para o vetor criado;
+ * OBBS: Se houver erro na alocação, imprime uma mesnagem informando que houve erro e encerra o programa;
+ */
+int *CriaVetor(int tamanho);
+
+/**
+ * @brief Lê um vetor de inteiros de tamanho especificado;
+ * 
+ * @param vetor Ponteiro para o vetor a ser lido;
+ * @param tamanho Tamanho do vetor a ser lido;
+ */
+void LeVetor(int *vetor, int tamanho);
+
+/**
+ * @brief Calcula a média dos valores de um vetor de inteiors com tamanho especificado;
+ * 
+ * @param vetor Ponteiro para o vetor a ser lido;
+ * @param tamanho Tamanho do vetor a ser lido;
+ * @return float Média dos valores do vetor;
+ */
+float CalculaMedia (int *vetor, int tamanho);
+
+/**
+ * @brief Libera a memória alocada para um vetor de inteiros;
+ * 
+ * @param vetor Ponteiro pata o vetor a ser liberado;
+ */
+void LiberaVetor(int *vetor);
+
+#endif
