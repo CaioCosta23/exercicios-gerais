042c33861aea5493f1c9f8b1d1ccf0f7c22c3e26
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2023-12-26 21:05:12 -0300

    Funcoes da biblioteca de eleicao completas e corrigidas

--- 04_TAD_simples/TAD_02/Respostas/CaioCostaLopes/eleicao.c
+++ 04_TAD_simples/TAD_02/Respostas/CaioCostaLopes/eleicao.c
@@ -18,61 +18,56 @@ tEleicao InicializaEleicao() {
     for (c = 0; c < qtdCandidatos; c++) {
         candidato[c] = LeCandidato();
 
-        if (candidato[c].cargo = 'G') {
+        if (candidato[c].cargo == 'G') {
             eleicao.governadores[g] = candidato[c];
             g++;
         }else {
-            if (candidato[c].cargo = 'P') {
-                if (g > 0){
-                    for (v = 0; v < g; v++) {
-                        if (EhMesmoCandidato(candidato[c], eleicao.governadores[v])) {
-                            printf("ELEICAO ANULADA\n");
-                            exit(1);
-                        }
-                    }
-                }
-                eleicao.governadores[p] = candidato[c];
+            if (candidato[c].cargo == 'P') {
+                eleicao.presidentes[p] = candidato[c];
                 p++;
             }
         }
     }
-    eleicao.totalGovernadores = g + 1;
-    eleicao.totalPresidentes = p + 1;
+    eleicao.totalGovernadores = g;
+    eleicao.totalPresidentes = p;
 
-    if ((eleicao.totalGovernadores > 3) || (eleicao.totalPresidentes > 3)) {
+    if ((eleicao.totalGovernadores > MAX_CANDIDATOS_POR_CARGO) || (eleicao.totalPresidentes > MAX_CANDIDATOS_POR_CARGO)) {
+        printf("ELEICAO ANULADA\n");
+        exit(1);
+    }
+    return eleicao;
+}
+
+tEleicao RealizaEleicao(tEleicao eleicao) {
+    int governadorValido = 0, presidenteValido = 0;
+    int e1, e2, g, p, qtdEleitores, v;
+
+    scanf("%d", &qtdEleitores);
+
+    if (qtdEleitores > MAX_ELEITORES) {
         printf("ELEICAO ANULADA\n");
         exit(1);
     }
 
-    return eleicao;
-}
-
-tEleicao Realizaeleicao(tEleicao eleicao) {
-    int governadorValido, presidenteValido;
-    int v = 0;
-    int e1, e2, g, p;
-    tEleitor eleitor[MAX_ELEITORES];
-
-    while(1) {
-        eleitor[v] = LeEleitor();
-
-        if (v > 0) {
-            for (e1 = 0; e1 < (v - 1); e1++) {
-                for (e2 = e1 + 1; e2 < v; e2++) {
-                    if (EhMesmoEleitor(eleitor[e1], eleitor[e2])) {
-                        printf("ELEICAO ANULADA\n");
-                        exit(1);
-                    }
-                }
+    for (v = 0; v < qtdEleitores; v++) {
+        eleicao.eleitores[v] = LeEleitor();
+    }
+    for (e1 = 0; e1 < (qtdEleitores - 1); e1++) {
+        for (e2 = e1 + 1; e2 < qtdEleitores; e2++) {
+            if (EhMesmoEleitor(eleicao.eleitores[e1], eleicao.eleitores[e2])) {
+                printf("ELEICAO ANULADA\n");
+                exit(1);
             }
         }
+    }
 
-        if (ObtemVotoGoverador(eleitor[v]) == 0) {
+    for (v = 0; v < qtdEleitores; v++) {
+        if (ObtemVotoGovernador(eleicao.eleitores[v]) == 0) {
             eleicao.votosBrancosGovernador += 1;
         }else {
             governadorValido = 0;
             for (g = 0; g < eleicao.totalGovernadores; g++) {
-                if (VerificaIdCandidato(eleicao.governadores[g], ObtemVotoGoverador(eleitor[v]))) {
+                if (VerificaIdCandidato(eleicao.governadores[g], ObtemVotoGovernador(eleicao.eleitores[v]))) {
                     eleicao.governadores[g] = IncrementaVotoCandidato(eleicao.governadores[g]);
                     governadorValido = 1;
                     break;
@@ -82,38 +77,32 @@ tEleicao Realizaeleicao(tEleicao eleicao) {
                 eleicao.votosNulosGovernador += 1;
             }
         }
-        if (ObtemVotoPresidente(eleitor[v]) == 0) {
+        if (ObtemVotoPresidente(eleicao.eleitores[v]) == 0) {
             eleicao.votosBrancosPresidente += 1;
         }else {
             presidenteValido = 0;
             for (p = 0; p < eleicao.totalPresidentes; p++) {
-                if (VerificaIdCandidato(eleicao.presidentes[p], ObtemVotoPresidente(eleitor[v]))) {
+                if (VerificaIdCandidato(eleicao.presidentes[p], ObtemVotoPresidente(eleicao.eleitores[v]))) {
                     eleicao.presidentes[p] = IncrementaVotoCandidato(eleicao.presidentes[p]);
                     presidenteValido = 1;
                     break;
                 }
             }
+            
             if (!(presidenteValido)) {
                 eleicao.votosNulosPresidente += 1;
             }
         }
-
-        v++;
-
-        if (v > 10) {
-            printf("ELEICAO ANULADA\n");
-            exit (1);
-        }
     }
+    return eleicao;
 }
 
-void ImprimeResultadosEleicao(tEleicao eleicao) {
+void ImprimeResultadoEleicao(tEleicao eleicao) {
     int v, totalVotosGov, totalVotosPres;
     int votosValidosGov = 0, votosValidosPres = 0;
-    int empateGovernadores = 0, empatePresitentes = 0;
+    int empateGovernadores = 0, empatePresidentes = 0;
     tCandidato liderGovernador, liderPresidente;
 
-    printf("- PRESIDENTE ELEITO: ");
     for (v = 0; v < eleicao.totalPresidentes; v++) {
         votosValidosPres += ObtemVotos(eleicao.presidentes[v]);
     }
@@ -121,29 +110,29 @@ void ImprimeResultadosEleicao(tEleicao eleicao) {
     for (v = 0; v < eleicao.totalPresidentes; v++) {
         if (v == 0) {
             liderPresidente = eleicao.presidentes[v];
-        }
-        if ((ObtemVotos(eleicao.presidentes[v])) > liderPresidente.votos) {
-            liderPresidente = eleicao.presidentes[v];
-            empatePresitentes = 0;
-        }else {
-            if ((ObtemVotos(eleicao.presidentes[v])) == liderPresidente.votos) {
-                empatePresitentes = 1;
+        }else{
+            if ((ObtemVotos(eleicao.presidentes[v])) > liderPresidente.votos) {
+                liderPresidente = eleicao.presidentes[v];
+                empatePresidentes = 0;
+            }else {
+                if ((ObtemVotos(eleicao.presidentes[v])) == liderPresidente.votos) {
+                    empatePresidentes = 1;
+                }
             }
         }
     }
 
+    printf("- PRESIDENTE ELEITO: ");
     if (ObtemVotos(liderPresidente) < (eleicao.votosBrancosPresidente + eleicao.votosNulosPresidente)) {
-        printf("SEM DECISAO");
+        printf("SEM DECISAO\n");
     }else {
-        if (empatePresitentes) {
-            printf("EMPATE. SERA NECESSARIO UMA NOVA VOTACAO");
+        if (empatePresidentes) {
+            printf("EMPATE. SERA NECESSARIO UMA NOVA VOTACAO\n");
         }else {
             ImprimeCandidato(liderPresidente, CalculaPercentualVotos(liderPresidente, totalVotosPres));
         }
     }
-    printf("\n");
 
-    printf("- GOVERNADOR ELEITO: ");
     for (v = 0; v < eleicao.totalGovernadores; v++) {
         votosValidosGov += ObtemVotos(eleicao.governadores[v]);
     }
@@ -151,27 +140,27 @@ void ImprimeResultadosEleicao(tEleicao eleicao) {
     for (v = 0; v < eleicao.totalGovernadores; v++) {
         if (v == 0) {
             liderGovernador = eleicao.governadores[v];
-        }
-        if ((ObtemVotos(eleicao.governadores[v])) > liderGovernador.votos) {
-            liderGovernador = eleicao.governadores[v];
-            empateGovernadores = 0;
-        }else {
-            if ((ObtemVotos(eleicao.governadores[v])) == liderGovernador.votos) {
-                empateGovernadores = 1;
+        }else{
+            if ((ObtemVotos(eleicao.governadores[v])) > liderGovernador.votos) {
+                liderGovernador = eleicao.governadores[v];
+                empateGovernadores = 0;
+            }else {
+                if ((ObtemVotos(eleicao.governadores[v])) == liderGovernador.votos) {
+                    empateGovernadores = 1;
+                }
             }
         }
     }
 
+    printf("- GOVERNADOR ELEITO: ");
     if (ObtemVotos(liderGovernador) < (eleicao.votosBrancosGovernador + eleicao.votosNulosGovernador)) {
-        printf("SEM DECISAO");
+        printf("SEM DECISAO\n");
     }else {
         if (empateGovernadores) {
-            printf("EMPATE. SERA NECESSARIO UMA NOVA VOTACAO");
+            printf("EMPATE. SERA NECESSARIO UMA NOVA VOTACAO\n");
         }else {
             ImprimeCandidato(liderGovernador, CalculaPercentualVotos(liderGovernador, totalVotosGov));
         }
     }
-    printf("\n");
-
-    printf("- NULOS E BRANCOS: %d %d", (eleicao.votosBrancosGovernador + eleicao.votosNulosGovernador), (eleicao.votosBrancosPresidente + eleicao.votosNulosPresidente));
+    printf("- NULOS E BRANCOS: %d, %d", (eleicao.votosNulosGovernador + eleicao.votosNulosPresidente), (eleicao.votosBrancosGovernador + eleicao.votosBrancosPresidente));
 }
