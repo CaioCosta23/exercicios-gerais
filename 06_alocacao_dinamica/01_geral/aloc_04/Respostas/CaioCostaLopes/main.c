23488ec1d6116770eacec25e690b18b1ee544795
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-03-13 18:02:49 -0300

    Adicionando main completa e corrigida

--- 06_alocacao_dinamica/01_geral/aloc_04/Respostas/CaioCostaLopes/main.c
+++ 06_alocacao_dinamica/01_geral/aloc_04/Respostas/CaioCostaLopes/main.c
@@ -4,16 +4,16 @@
 #include "utils_char2.h"
 
 int main() {
-    /*
+    
     char *vetor;
     int tamanho;
 
-    tamanho = strlen(vetor);
+    tamanho = TAM_PADRAO;
 
     vetor = CriaVetorTamPadrao();
     vetor = LeVetor(vetor, &tamanho);
     ImprimeString(vetor);
     LiberaVetor(vetor);
-    */
+    
     return 0;
 }
