baea781313e404e97419c455233b66f8d078d2fc
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-03-13 17:18:02 -0300

    Adicionando primeira versão do main.c

--- 06_alocacao_dinamica/01_geral/aloc_04/Respostas/CaioCostaLopes/main.c
+++ 06_alocacao_dinamica/01_geral/aloc_04/Respostas/CaioCostaLopes/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "utils_char2.h"
 
 int main() {
-    
+    /*
+    char *vetor;
+    int tamanho;
+
+    tamanho = strlen(vetor);
+
+    vetor = CriaVetorTamPadrao();
+    vetor = LeVetor(vetor, &tamanho);
+    ImprimeString(vetor);
+    LiberaVetor(vetor);
+    */
     return 0;
 }
