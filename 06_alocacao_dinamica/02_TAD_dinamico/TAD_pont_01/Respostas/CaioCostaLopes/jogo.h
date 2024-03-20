7673ebcabeb3c0d1296017b0a5c35701b3c78b4d
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-03-20 17:45:25 -0300

    Corrigindo mais comentários das funções

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_01/Respostas/CaioCostaLopes/jogo.h
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_01/Respostas/CaioCostaLopes/jogo.h
@@ -12,22 +12,22 @@ typedef struct {
 }tJogo;
 
 /**
- * @brief Aloca e retona uma estrura do tipo "tJogo";
- *  Se a aocação falhar, o porama é encerrao;
+ * @brief Aloca e retona uma estrutura do tipo "tJogo";
+ *  Se a alocação falhar, o programa é encerrao;
  * 
- * @return tJogo* a esuura do tipo "tJogo" alocada;
+ * @return tJogo* a estrutura do tipo "tJogo" alocada;
  */
 tJogo *CriaJogo();
 
 /**
- * @brief Inicia o jogo definindo o tabuleio e os jogadores;
+ * @brief Inicia o jogo definindo o tabuleiro e os jogadores;
  * 
  * @param jogo O jogo a ser iniciado;
  */
 void ComecaJogo(tJogo *jogo);
 
 /**
- * @brief Vrifica se o jogo acabou( se não há mais posições livres no tabuleiro);
+ * @brief Verifica se o jogo acabou (se não há mais posições livres no tabuleiro);
  * 
  * @param jogo O jogo atual;
  * @return int 1 se o joo acabou, 0 caso contrário;
@@ -35,16 +35,16 @@ void ComecaJogo(tJogo *jogo);
 int AcabouJogo (tJogo *jogo);
 
 /**
- * @brief Veifica se o usuáio deseja jogar novamente;
+ * @brief Veifica se o usuário deseja jogar novamente;
  * 
  * @return int 1 se  o usuário deseja jogar novamene, 0 caso contrário;
  */
 int ConinuaJogo();
 
 /**
- * @brief Libera a memória de uma estura do tipo "tJogo";
+ * @brief Libera a memória de uma estrutura do tipo "tJogo";
  * 
- * @param jogo A estrutua do tipo "tJogo" a ser liberada;
+ * @param jogo A estrutura do tipo "tJogo" a ser liberada;
  */
 void DestroiJogo(tJogo *jogo);
 
