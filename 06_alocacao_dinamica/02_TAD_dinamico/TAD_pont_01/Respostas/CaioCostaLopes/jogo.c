645b6f21b7f60c69fac3de56d51c93f41e307053
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-03-21 12:00:26 -0300

    Adicionando comentários ao arquivo.c da biblioteca do jogo

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_01/Respostas/CaioCostaLopes/jogo.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_01/Respostas/CaioCostaLopes/jogo.c
@@ -13,7 +13,7 @@ tJogo *CriaJogo() {
         printf("Erro na alocacao!");
         exit(1);
     }
-
+    // Cria e atribui o tabuleiro e os jogadores a cada dado da estrutura do jogo;
     jogo->tabuleiro = CriaTabuleiro();
     jogo->jogador1 = CriaJogador(PECA_1);
     jogo->jogador2 = CriaJogador(PECA_2);
@@ -30,18 +30,23 @@ int AcabouJogo(tJogo *jogo) {
 
 void ComecaJogo(tJogo *jogo) {
     while (1) {
+        // Vez do Jogador 1 realizar sua jogada;
         JogaJogador((*jogo).jogador1, (*jogo).tabuleiro);
+        // A cada jogada (independente de qual jogador fez a jogada), o tabuleiro é impresso na tela com o estado atual do jogo;
         ImprimeTabuleiro((*jogo).tabuleiro);
 
+        // Verifica se o jogador 1 venceu e, caso sim, informa ao usuário do programa com uma mensagem encerra o jogo atual;
         if(VenceuJogador((*jogo).jogador1, (*jogo).tabuleiro)) {
             printf("JOGADOR %d Venceu!\n", PECA_1);
             break;
         }
+        // Verifica se o jogo sem um vencedor;
         if(AcabouJogo(jogo)) {
             printf("Sem vencedor!\n");
             break;
         }
 
+        // Verifica se o jogador 1 venceu e, caso sim, informa ao usuário do programa com uma mensagem encerra o jogo atual;
         JogaJogador((*jogo).jogador2, (*jogo).tabuleiro);
         ImprimeTabuleiro((*jogo).tabuleiro);
 
@@ -61,11 +66,14 @@ int ContinuaJogo() {
 
     printf("Jogar novamente? (s,n) ");
 
+    // Consome tudo que o usuário digitar que seja diferente de 's' ou 'n';
     scanf("%*[^sn]");
+    // Caso o usuário digite um 's' ou 'n', a varíável coleta a informação;
     scanf("%c", &opcao);
 
     printf("\n");
 
+    // Se 's' (sim), o programa continuará rodando, se 'n' (não) a função retorna falso (0), indicando que o jogo não continuará (assim encerrando o programa);
     if (opcao == 's') {
         return 1;
     }
@@ -74,8 +82,11 @@ int ContinuaJogo() {
 }
 
 void DestroiJogo(tJogo *jogo) {
-    free((*jogo).tabuleiro);
-    free((*jogo).jogador1);
-    free((*jogo).jogador2);
+    // Chama as funções para destruir/ desalocar cada dado pertencente a estrutua do jogo;
+    DestroiTabuleiro((*jogo).tabuleiro);
+    DestroiJogador((*jogo).jogador1);
+    DestroiJogador((*jogo).jogador2);
+
+    // Desaloca a estrutura do jogo;
     free(jogo);
 }
