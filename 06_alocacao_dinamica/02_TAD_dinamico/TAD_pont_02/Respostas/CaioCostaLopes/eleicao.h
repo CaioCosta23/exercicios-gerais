0001b2052e4dcf1e8d642a28bea34e8ed3b3f391
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-03-27 15:53:41 -0300

    Adicionando arquivo de biblioteca da eleição

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_02/Respostas/CaioCostaLopes/eleicao.h
@@ -0,0 +1,56 @@
+#ifndef _ELEICAO_H_
+#define _ELEICAO_H_
+
+#include "candidato.h"
+#include "eleitor.h"
+
+// #define MAX_CANDIDATOS_PORCARGO 3
+// #define MAX_ELEITORES 10
+
+
+typedef struct {
+    tCandidato **presidentes;
+    int totalPresidentes;
+    tCandidato **governadores;
+    int totalGovernadores;
+    int votosBrancosPresidente;
+    int votosNulosPresidente;
+    int VotosBrancosGovernador;
+    int vootosNulosGovernador;
+    tEleitor **eleitores;
+    int totaleleitores;
+}tEleicao;
+
+/**
+ * @brief Inicializa uma eleição com valores padrão (zerando as variáveis que armazenam votos).
+ * Ainda Nessa função, é lido a quantidade de candidats e os cadidatos são lidos e armazenados.
+ * A memória necessária para os vetores dos "presidentes" e dos "governadores" deve ser alocada nesta função.
+ * Pr fim, os demais ponteiros devem ser inicializados com NULL;
+ * 
+ * @return tEleicao* Eleicao inicializada;
+ */
+tEleicao *InicializaEleicao();
+
+/**
+ * @brief Apaga e desaloca os dados da eleição;
+ * 
+ * @param eleicao Ponteiro para a eleição;
+ */
+void ApagaEleicao(tEleicao *eleicao);
+
+/**
+ * @brief Realiza uma eleição;
+ * Nessa função, é lida a quantidade de eleitores, sendo que estes tem os seus dados lidos e armazenados;
+ * 
+ * @param eleicao Ponteiro para a eleição a ser realizada;
+ */
+void RealizaEleicao(tEleicao *eleicao);
+
+/**
+ * @brief Imprime o resultado da eleição na tela à partir da apuração dos votos;
+ * 
+ * @param eleicao Eleição a ser impressa;
+ */
+void ImprimeResultadoEleicao(tEleicao *eleicao);
+
+#endif
