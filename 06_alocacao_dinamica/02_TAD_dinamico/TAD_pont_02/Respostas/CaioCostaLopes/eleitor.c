4e8648bfae0da7b1b6fed553b75d9ca8ebe24b1b
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-03-27 18:26:43 -0300

    Adicionando arquivo .c do eleitor (com erros de resultados)

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_02/Respostas/CaioCostaLopes/eleitor.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "eleitor.h"
+
+#define MAX_ELEITORES 10
+
+tEleitor *CriaEleitor() {
+    tEleitor *eleitor = (tEleitor *) malloc(sizeof(tEleitor));
+
+    if (eleitor == NULL) {
+        printf("Erro na alocacao da estrutura de dados do eleitor!");
+    }
+
+    eleitor->id = -1;
+    eleitor->votoG = -1;
+    eleitor->votoP = -1;
+
+    return eleitor;
+}
+
+void LeEleitor(tEleitor *eleitor) {
+
+    scanf("%d %d %d", &(*eleitor).id, &(*eleitor).votoG, &(*eleitor).votoP);
+}
+
+int ObtemVotoPresidente(tEleitor *eleitor) {
+    return (*eleitor).votoP;
+}
+
+int ObtemVotoGovernador(tEleitor *eleitor) {
+    return (*eleitor).votoG;
+}
+
+int EhMesmoEleitor(tEleitor *eleitor1, tEleitor *eleitor2) {
+    return ((*eleitor1).id == (*eleitor2).id);
+}
+
+void ApagaEleitor(tEleitor *eleitor) {
+    free(eleitor);
+}
