38b78ad27d12314bec0d06b59a020a2a89aff601
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-03-30 19:09:34 -0300

    Adicionando primeira versao do arquivo.c da eleicao (função de inicializar eleição feita)

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_02/Respostas/CaioCostaLopes/eleicao.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdio.h>
+#include "eleicao.h"
+
+#define MAX_CANDIDATOS_POR_CARGO 3
+#define MAX_ELEITORES 10
+
+tEleicao *InicializaEleicao() {
+    tEleicao *eleicao = (tEleicao *)malloc(sizeof(tEleicao));
+    tCandidato *candidato = NULL;
+
+    if (eleicao == NULL) {
+        printf("Erro na alocacao da eleicao!\n");
+        exit(1);
+    }
+
+    int qtdCandidatos, c;
+
+    eleicao->votosBrancosGovernador = 0;
+    eleicao->votosBrancosPresidente = 0;
+    eleicao->votosNulosGovernador = 0;
+    eleicao->votosNulosPresidente = 0;
+
+    scanf("%d", &qtdCandidatos);
+
+    eleicao->presidentes = (tCandidato *)malloc(MAX_CANDIDATOS_POR_CARGO * sizeof(tCandidato));
+    if ((*eleicao).presidentes == NULL) {
+        printf("Erro de alocacao no vetor dos presidentes!\n");
+        exit(1);
+    }
+    eleicao->governadores = (tCandidato *)malloc(MAX_CANDIDATOS_POR_CARGO * sizeof(tCandidato));
+    if ((*eleicao).governadores == NULL) {
+        printf("Erro de alocacao no vetor dos governadores!\n");
+        exit(1);
+    }
+
+    for (c = 0; c < qtdCandidatos; c++) {
+        candidato = CriaCandidato();
+
+        if (ObtemCargo(candidato) == 'G') {
+            eleicao->totalGovernadores += 1;
+            eleicao->governadores[(*eleicao).totalGovernadores] = candidato;
+        }else {
+            if (ObtemCargo(candidato) == 'P') {
+                eleicao->totalPresidentes += 1;
+                eleicao->presidentes[(*eleicao).totalPresidentes] = candidato;    
+            }
+        }
+
+        eleicao->presidentes = realloc((*eleicao).presidentes, (*eleicao).totalPresidentes);
+        if ((*eleicao).presidentes == NULL) {
+            printf("Erro na realocacao do vetor dos candidatos a presidente!\n");
+            exit(1);
+        }
+        eleicao->governadores = realloc((*eleicao).governadores, (*eleicao).totalGovernadores);
+        if ((*eleicao).governadores == NULL) {
+            printf("erro na realocacao do vetor dos candidatos a governador!\n");
+            exit(1);
+        }
+
+        if (((*eleicao).totalGovernadores > MAX_CANDIDATOS_POR_CARGO) || ((*eleicao).totalPresidentes) > MAX_CANDIDATOS_POR_CARGO) {
+            printf("ELEICAO ANULADA\n");
+            exit(1);
+        }
+    }
+}
+
