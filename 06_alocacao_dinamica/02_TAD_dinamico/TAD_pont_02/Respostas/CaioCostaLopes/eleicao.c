9f4d89e22aa841ce1161705dc89cfafda23fc0de
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-03-30 19:47:40 -0300

    Adicionando funcao de apagar eleicao

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_02/Respostas/CaioCostaLopes/eleicao.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_02/Respostas/CaioCostaLopes/eleicao.c
@@ -14,6 +14,13 @@ tEleicao *InicializaEleicao() {
         exit(1);
     }
 
+    eleicao->eleitores = (tEleitor *)malloc(MAX_ELEITORES * sizeof(tEleitor));
+    
+    if ((*eleicao).eleitores == NULL) {
+        printf("Erro na alocacao do vetor de eleitores!\n");
+        exit(1);
+    }
+
     int qtdCandidatos, c;
 
     eleicao->votosBrancosGovernador = 0;
@@ -59,6 +66,7 @@ tEleicao *InicializaEleicao() {
         }
 
         if (((*eleicao).totalGovernadores > MAX_CANDIDATOS_POR_CARGO) || ((*eleicao).totalPresidentes) > MAX_CANDIDATOS_POR_CARGO) {
+            ApagaEleicao(eleicao);
             printf("ELEICAO ANULADA\n");
             exit(1);
         }
@@ -67,3 +75,41 @@ tEleicao *InicializaEleicao() {
     return eleicao;
 }
 
+void RealizaEleicao(tEleicao *eleicao) {
+    int governadorValido = 0, presidenteValido = 0;
+    int e1, e2, g, p, qtdEleitores, v;
+    tEleitor *eleitor = NULL;
+
+    scanf("%d", &qtdEleitores);
+
+    if (qtdEleitores > MAX_ELEITORES) {
+        ApagaEleicao(eleicao);
+        printf("ELEICAO ANULADA\n");
+        exit(1);
+    }
+
+    eleicao->eleitores = realloc((*eleicao).eleitores, qtdEleitores);
+
+    for (v = 0; v < qtdEleitores; v++) {
+        eleitor = Criaeleitor();
+    }
+}
+
+void ApagaEleicao(tEleicao *eleicao) {
+    int g, p, e;
+    
+    for (g = 0; g < (*eleicao).totalGovernadores; g++) {
+        ApagaCandidato((*eleicao).governadores[g]);
+    }
+    for (p = 0; p < (*eleicao).totalPresidentes; p++) {
+        ApagaCandidato((*eleicao).presidentes[p]);
+    }
+    for (e = 0; e < (*eleicao).totalEleitores; e++) {
+        ApagaEleitor((*eleicao).eleitores[e]);
+    }
+
+    free((*eleicao).governadores);
+    free((*eleicao).presidentes);
+    free((*eleicao).eleitores);
+    free(eleicao);
+}
