f69e88c5eba558393f165f03f8414fad7f1aa821
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-04-11 15:24:57 -0300

    Adicionando versão comentada da main.c

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_03/Respostas/CaioCostaLopes/main.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_03/Respostas/CaioCostaLopes/main.c
@@ -13,13 +13,17 @@ int main() {
     char operacao[TAM_STRING_OPERACAO];
     char opcoes[NUM_OPCOES][TAM_STRING_OPERACAO] = {"Cadastrar", "Alugar", "Devolver", "Estoque"};
 
+    // Cria (aloca dinâmicamente) a locadora;
     locadora = CriarLocadora();
 
+    // Lê a ação que o usuário deseja realizar;
     while(scanf("%s", operacao) == 1) {
+        // verifica qual das 4 ações o usuário escolheu e através das funções abaixo as realiza;
         if (strcmp(operacao, opcoes[0]) == 0) {
             LerCadastroLocadora(locadora);
             printf("\n");
 
+            // Verifica se o usuário deseja parar de cadastrar filmes (através do caractere de parada - '#');
             if ((scanf("%c", &parada) == 1) && (parada == '#')) {
                 continue;
             }
@@ -49,8 +53,10 @@ int main() {
             }
         }
     }
+    // Exibe o lucro total que a locadora obteve com as ações;
     ConsultarLucroLocadora(locadora);
-
+    
+    // Destrói (desaloca) a locadora;
     DestruirLocadora(locadora);
 
     return 0;
