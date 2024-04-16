fa47e8fd0564f6b6adb7525ff96e080d27ab77dd
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-04-16 12:02:28 -0300

    Adicionando primeira versão da main

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_04/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "aluno.h"
+
+void OrdenaAprovados(tAluno *aprovados[], int p) {
+    int x, y;
+    int *aux;
+
+    for (x = 0; x < (p - 1); x++) {
+        for (y = x + 1; y < p; y++) {
+            if (ComparaMatricula(aprovados[x], aprovados[y]) == 1) {
+                aux = aprovados[y];
+                aprovados[y] = aprovados[x];
+                aprovados[x] = aux;
+            }
+        }
+    }
+}
+
+
+int main() {
+    int qtdAlunos;
+    int a, i;
+    int p = 0;
+
+    scanf("%d\n", &qtdAlunos);
+
+    tAluno **alunos = (tAluno**)malloc(qtdAlunos * sizeof(tAluno*));
+    if (alunos == NULL) {
+        printf("Erro na alocacao do vetor de alunos!\n");
+        exit(1);
+    }
+
+    tAluno **aprovados = (tAluno**)malloc(qtdAlunos * sizeof(tAluno*));
+    if (aprovados == NULL) {
+        printf("Erro na alocacao do vertor de alunos dos aprovados");
+    }
+
+    for (a = 0; a < qtdAlunos; a++) {
+        alunos[a] = CriaAluno();
+        LeAluno(alunos[a]);
+
+        if (VerificaAprovacao(alunos[a])) {
+            aprovados = alunos[a];
+            p++;
+        }
+    }
+
+    aprovados = (tAluno **)realloc(aprovados, p * (sizeof(tAluno*)));
+
+    OrdenaAprovados(aprovados, p);
+
+    for (i = 0; i < p; i++) {
+        ImprimeAluno(aprovados[i]);
+    }
+    printf("\n");
+
+    for (a = 0; a < qtdAlunos; a++) {
+        ApagaAluno(alunos[a]);
+    }
+    free(alunos);
+    free(aprovados);
+
+    return 0;
+}
