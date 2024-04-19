c0be945ecc709ee577a0e95aceb369180e12bde9
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-04-19 14:02:52 -0300

    Adicionando primeira versão do arquivo .c da biblioteca data

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_07/Respostas/CaioCostaLopes/data.c
@@ -0,0 +1,184 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "data.h"
+
+tData *CriaData() {
+    tData *data = (tData*)malloc(sizeof(tData));
+
+    if (data == NULL) {
+        printf("Erro na alocacao da data!\n");
+        exit(1);
+    }
+
+    return data;
+}
+
+void LeData(tData *data) {
+    scanf("%02d/%02d/%04d", &data->dia, &data->mes, &data->ano);
+}
+
+int VerificaDataValida(tData *data) {
+    if ((*data).ano > 0) {
+        if (((*data).mes > 0) && ((*data).mes <= 12)) {
+            if (((*data).dia > 0) && ((*data).dia <= NumeroDiasMes(data))) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int VerificaBissexto(tData *data) {
+    return ((((*data).ano % 4 == 0) && ((*data).ano % 100 != 0)) || (((*data).ano % 100 == 0) && ((*data).ano % 400 == 0)));
+}
+
+int NumeroDiasMes(tData *data) {
+    if (((*data).mes == 1) || ((*data).mes == 3) || ((*data).mes == 5) || ((*data).mes == 7) || ((*data).mes == 8)
+    || ((*data).mes == 10) || ((*data).mes == 12)) {
+        return 31;
+    }else {
+        if (((*data).mes == 4) || ((*data).mes == 6) || ((*data).mes == 9) || ((*data).mes == 11)) {
+            return 30;
+        }else {
+            if (VerificaBissexto(data)) {
+                return 29;
+            }else {
+                return 28;
+            }
+        }
+    }
+}
+
+int ComparaData(tData *data1, tData *data2) {
+    if ((*data1).ano > (*data2).ano) {
+        return 1;
+    }else {
+        if (((*data1).ano == (*data2).ano) && ((*data1).mes) > (*data2).mes) {
+            return 1;
+        }else {
+            if (((*data1).mes == (*data2).mes) && ((*data1).dia > (*data2).dia)) {
+                return 1;
+            }else {
+                if ((*data1).dia == (*data2).dia) {
+                    return 0;
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+int CalculaDiasAteMes(tData *data) {
+    return NumeroDiasAteMes(data);
+}
+
+int CalculaDiferencaDias(tData *data1, tData *data2) {
+    int diferenca = 0;
+    tData *aux;
+    
+    aux->dia = 0;
+
+    if (ComparaData(data1, data2) == 0) {
+        return 0;
+    }else {
+        if (ComparaData(data1, data2) == -1) {
+            if ((*data1).dia < (*data2).dia) {
+                diferenca += ((*data2).dia - (*data1).dia);
+            }else {
+                /*
+                 *Calcula a diferenca de dias caso o dia da data que esta mais a frente seja menor que o dia da data que esta mais no atras;
+                 * Um exemplo - 25/10/2023 a 23/11/2023. Sabendo que Outubro tem 31 dias (calculado na funcao "calculaDiasAteMes()"), temos entao:
+                 * diferenca = diferenca + (31 - 25) + 23);
+                 */
+                diferenca += ((CalculaDiasAteMes(data1) - (*data1).dia) + (*data2).dia);
+            }
+            aux->mes = (*data1).mes;
+            for ((*aux).ano = (*data1).ano; (*aux).ano <= (*data2).ano; (*aux).ano++) {
+                while (1) {
+                    aux->mes++;
+
+                    if (((*aux).mes > 12) || (((*data1).ano == (*data2).ano) && ((*aux).mes >= (*data2).mes))) {
+                        break;
+                    }
+                    diferenca += CalculaDiasAteMes(aux);
+                }
+                aux->mes = 0;
+            }
+        }else {
+            if ((*data2).dia < (*data1).dia) {
+                diferenca += ((*data1).dia - (*data2).dia);
+            }else {
+                /**
+                 * @brief Calcula a diferenca de dias caso o dia da data que esta mais a frente seja menor que o dia da data que esta mais no atras;
+                 * Um exemplo - 25/10/2023 a 23/11/2023. Sabendo que Outubro tem 31 dias (calculado na funcao "calculaDiasAteMes()"), temos entao:
+                 * diferenca = diferenca + (31 - 25) + 23);
+                 */
+                diferenca += ((CalculaDiasAteMes(data2) - (*data2).dia) + (*data1).dia);
+            }
+            (*aux).mes = (*data2).mes;
+            for ((*aux).ano = (*data2).ano; (*aux).ano <= (*data1).ano; (*aux).ano++) {
+                while (1) {
+                    aux->mes++;
+
+                    if (((*aux).mes > 12) || (((*data1).ano == (*data2).ano) && ((*aux).mes >= (*data1).mes))) {
+                        break;
+                    }
+                    diferenca += CalculaDiasAteMes(aux);
+                }
+                aux->mes = 0;
+            }
+        }
+    }
+    return diferenca;
+}
+
+void ImprimeDataExtenso(tData *data) {
+    printf("%02d de ", (*data).dia);
+    ImprimeMesExtenso(data);
+    printf(" de %04d\n", (*data).ano);
+}
+
+void ImprimeMesExtenso(tData *data) {
+    switch((*data).mes) {
+        case 1:
+            printf("Janeiro");
+            break;
+        case 2:
+            printf("Fevereiro");
+            break;
+        case 3:
+            printf("Março");
+            break;
+        case 4:
+            printf("Abril");
+            break;
+        case 5:
+            printf("Maio");
+            break;
+        case 6:
+            printf("Junho");
+            break;
+        case 7:
+            printf("Julho");
+            break;
+        case 8:
+            printf("Agosto");
+            break;
+        case 9:
+            printf("Setembro");
+            break;
+        case 10:
+            printf("Outubro");
+            break;
+        case 11:
+            printf("Novembro");
+            break;
+        case 12:
+            printf("Dezembro");
+            break;
+    }
+}
+
+void LiberaData(tData *data) {
+    free(data);
+}
