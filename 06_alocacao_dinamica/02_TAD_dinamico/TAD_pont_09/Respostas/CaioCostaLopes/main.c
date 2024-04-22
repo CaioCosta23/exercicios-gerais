c5622a8306633d499f9b6640b37e2952ea079a0d
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-04-22 15:11:41 -0300

    Adicionando primeira versão da main.c

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_09/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "empresa.h"
+
+int main() {
+    int qtdEmpresas, e;
+
+    scanf("%d", &qtdEmpresas);
+
+    tEmpresa *empresas[qtdEmpresas];
+
+    for (e = 0; e < qtdEmpresas; e++) {
+        LeEmpresa(empresas[e]);
+    }
+    for (e = 0; e < qtdEmpresas; e++) {
+        ImprimeEmpresa(empresas[e]);
+    }
+
+    for(e = 0; e < qtdEmpresas; e++) {
+        ApagaEmpresa(empresas[e]);
+    }
+
+    return 0;
+}
