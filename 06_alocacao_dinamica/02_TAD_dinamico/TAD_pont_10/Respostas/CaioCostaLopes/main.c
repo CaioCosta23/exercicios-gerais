3888d7a36daa41edf212597946ba891f6bf384d2
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-04-23 13:23:36 -0300

    reformulando condicionais de opção de escolha, adicionando opção de falha de alocação e corrigindo adição de livro a biblioteca

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_10/Respostas/CaioCostaLopes/main.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_10/Respostas/CaioCostaLopes/main.c
@@ -4,6 +4,7 @@
 
 int main() {
     tBiblioteca *biblioteca;
+    tLivros *livro;
     int qtdOperacoes, opcao, r;
     char titulo[50];
 
@@ -13,28 +14,36 @@ int main() {
 
     printf("Lista de operacoes:\n");
     printf("1 - Cadastrar livros.\n");
-    printf("2 - Remover livros\n");
-    printf("3 - Listar todos os livros cadastrados\n\n");
+    printf("2 - Remover livros.\n");
+    printf("3 - Listar todos os livros cadastrados.\n\n");
 
     for (r = 0; r < qtdOperacoes; r++) {
         scanf("%d", &opcao);
 
-        if (opcao == 1) {
-            AdicionarLivroNaBiblioteca(biblioteca);
-        }else {
-            if (opcao == 2) {
-                scanf("%s", titulo);
-                RemoverLivroDaBiblioteca(biblioteca, titulo);
-            }else {
-                if (opcao == 3) {
-                    ListarLivrosDaBiblioteca(biblioteca);
-                }else {
-                    printf("Operacao invalida!\n");
+        switch(opcao) {
+            case 1:
+                livro = CriaLivro();
+                if (livro == NULL) {
+                    ApagaBiblioteca(biblioteca);
                     exit(1);
                 }
-            }
+                LerLivro(livro);
+                AdicionarLivroNaBiblioteca(biblioteca, livro);
+                break;
+            case 2:
+                scanf("%s", titulo);
+                RemoverLivroDaBiblioteca(biblioteca, titulo);
+                break;
+            case 3:
+                ListarLivrosDaBiblioteca(biblioteca);
+                break;
+            default:
+                printf("Operacao invalida!\n");
+                ApagaBiblioteca(biblioteca);
+                exit(1);
         }
     }
+    ApagaBiblioteca(biblioteca);
 
     return 0;
 }
