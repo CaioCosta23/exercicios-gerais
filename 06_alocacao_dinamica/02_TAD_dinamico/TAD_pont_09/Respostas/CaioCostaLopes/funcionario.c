5fdd742e241f4b86820034784883545fd629a2a3
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-04-23 10:27:28 -0300

    Adicioando primeira versão do arquivo .c do funcionario

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_09/Respostas/CaioCostaLopes/funcionario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcionario.h"
+
+tFuncionario *CriaFuncionario() {
+    tFuncionario *funcionario = (tFuncionario*)malloc(sizeof(tFuncionario));
+
+    funcionario->id = -1;
+    funcionario->salario = -1;
+
+    return funcionario;
+}
+
+void LeEmpresa(tFuncionario *funcionario) {
+    scanf("%d %f", &funcionario->id, &funcionario->salario);
+}
+
+int GEtIdFuncionario(tFuncionario *funcionario) {
+    return (*funcionario).id;
+}
+
+void ImprimeFuncionario(tFuncionario *funcionario) {
+    printf("- Funcionario %d: RS %.2f\n", (*funcionario).id, (*funcionario).salario);
+}
+
+void ApagaFuncionario(tFuncionario *funcionario) {
+    if (funcionario != NULL) {
+        free(funcionario);
+        funcionario = NULL;
+    }
+}
