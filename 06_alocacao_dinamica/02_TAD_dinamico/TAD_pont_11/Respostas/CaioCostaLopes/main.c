e71d86917fec3e78425848edbd5e3733fa67d912
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-04-29 19:59:11 -0300

    Adicionando arquivo main (completo)

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_11/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "loja.h"
+
+void FinalizaPrograma(tLoja *lojas[], int qtdLojas) {
+    int q;
+    for (q = 0; q < qtdLojas; q++) {
+        ApagaLoja(lojas[q]);
+    }
+}
+
+
+int main() {
+    int qtdLojas, opcao, idLoja, q;
+    int l = 0;
+    int encerrar = 0;
+    float aluguel, salario, comissao, valorVenda;
+    char nome[50];
+
+    scanf("%d", &qtdLojas);
+
+    tLoja *lojas[qtdLojas];
+
+    while (1) {
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 0:
+                encerrar = 1;
+                break;
+            case 1:
+                scanf("%d %f", &idLoja, &aluguel);
+                lojas[l] = AbreLoja(idLoja, aluguel);
+                if (lojas[l] == NULL) {
+                    FinalizaPrograma(lojas, l);
+                }
+                l++;
+                break;
+            case 2:
+                scanf("%s %f %f", nome, &salario, &comissao);
+                scanf("%d", &idLoja);
+
+                for (q = 0; q < l; q++) {
+                    if (VerificaIdLoja(lojas[q], idLoja)) {
+                        ContrataVendedor(lojas[q], RegistraVendedor(nome, salario, comissao));
+                        if (lojas[q] == NULL) {
+                            FinalizaPrograma(lojas, l);
+                        }
+                    }
+                }
+                break;
+            case 3:
+                scanf("%d %s %f", &idLoja, nome, &valorVenda);
+                for (q = 0; q < l; q++) {
+                    if (VerificaIdLoja(lojas[q], idLoja)) {
+                        RegistraVenda(lojas[q], nome, valorVenda);
+                    }
+                }
+                break;
+            case 4:
+                for (q = 0; q < l; q++) {
+                    CalculaLucro(lojas[q]);
+                    ImprimeRelatorioLoja(lojas[q]);
+                }
+                break;
+        }
+        if (encerrar) {
+            break;
+        }
+    }
+
+    FinalizaPrograma(lojas, l);
+
+    return 0;
+}
