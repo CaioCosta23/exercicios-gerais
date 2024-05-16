e8ca2cc4e3fb7d4eb3b01b1d060d89da03b217e1
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-05-16 13:00:00 -0300

    Adicionando arquivo .c da biblioteca do ponto

---
+++ 07_TAD_opaco/TAD_opac_00/Respostas/CaioCostaLopes/ponto.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "ponto.h"
+
+struct ponto {
+    float x, y;
+};
+
+tPonto Pto_Cria(float x, float y) {
+    tPonto ponto = (tPonto*)malloc(sizeof(tPonto));
+
+    if (ponto == NULL) {
+        printf("Erro na alocacao de memoria do ponto!\n");
+        return ponto;
+    }
+
+    Pto_Atribui_x(ponto, x);
+    Pto_Atribui_y(ponto, y);
+
+    return ponto;
+}
+
+float Pto_Acessa_x(tPonto p) {
+    return (*p).x;
+}
+
+float Pto_Acessa_y(tPonto p) {
+    return (*p).y;
+}
+
+void Pto_Atribui_x(tPonto p, float x) {
+    p->x = x;
+}
+
+void Pto_Atribui_y(tPonto p, float y) {
+    p->y = y;
+}
+
+float Pto_Distancia(tPonto p1, tPonto p2) {
+    return (sqrt((pow(((*p2).y - (*p1).y), 2)) + (pow(((*p2).x - (*p1).x), 2))));
+}
+
+void Pto_Apaga(tPonto p) {
+    if (p != NULL) {
+        free(p);
+        p = NULL;
+    }
+}
