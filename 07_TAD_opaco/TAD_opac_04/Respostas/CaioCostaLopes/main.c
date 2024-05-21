da5b9133221b3ac47fc6ba4a8c55cd4f1340506a
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-05-21 17:55:35 -0300

    Alterando para a versão de código antiga - TAD com Ponteiro (Exercicio 5 - Lista 6)

--- 07_TAD_opaco/TAD_opac_04/Respostas/CaioCostaLopes/main.c
+++ 07_TAD_opaco/TAD_opac_04/Respostas/CaioCostaLopes/main.c
@@ -2,66 +2,64 @@
 #include <stdlib.h>
 #include "aluno.h"
 
-void OrdenaAprovados(tAluno *aprovados[], int qtdAprovados) {
+void OrdenaAprovados(tAluno *aprovados[], int p) {
     int x, y;
-    tAluno *alunoAuxiliar;
+    tAluno *aux;
 
-    for (x = 0; x < (qtdAprovados - 1); x++) {
-        for (y = x + 1; x < qtdAprovados; y++) {
+    for (x = 0; x < (p - 1); x++) {
+        for (y = x + 1; y < p; y++) {
             if (ComparaMatricula(aprovados[x], aprovados[y]) == 1) {
-                alunoAuxiliar = aprovados[y];
+                aux = aprovados[y];
                 aprovados[y] = aprovados[x];
-                aprovados[x] = alunoAuxiliar;
+                aprovados[x] = aux;
             }
         }
     }
 }
 
 void DesalocaMemoria(tAluno *alunos[], int qtdAlunos) {
-    int a;
+    int i;
 
-    for (a = 0; a < qtdAlunos; a++) {
-        ApagaAluno(alunos[a]);
+    for (i = 0; i < qtdAlunos; i++) {
+        ApagaAluno(alunos[i]);
     }
 }
 
-int main () {
+
+int main() {
     int qtdAlunos;
     int a, i;
     int p = 0;
 
     scanf("%d\n", &qtdAlunos);
 
-    tAluno *alunos[qtdAlunos];
-    tAluno **aprovados =  NULL;
+    tAluno **alunos = (tAluno**)malloc(qtdAlunos * sizeof(tAluno*));
+    if (alunos == NULL) {
+        printf("Erro na alocacao do vetor de alunos!\n");
+        exit(1);
+    }
+
+    tAluno **aprovados = (tAluno**)malloc(qtdAlunos * sizeof(tAluno*));
+    if (aprovados == NULL) {
+        printf("Erro na alocacao do vertor de alunos dos aprovados");
+    }
 
     for (a = 0; a < qtdAlunos; a++) {
         alunos[a] = CriaAluno();
+        if (alunos[a] == NULL) {
+            DesalocaMemoria(alunos, a);
+        }
         LeAluno(alunos[a]);
+        if (alunos[a] == NULL) {
+            DesalocaMemoria(alunos, a);
+        }
 
         if (VerificaAprovacao(alunos[a])) {
-            if (p == 0) {
-                aprovados = (tAluno**)malloc((p + 1) * sizeof(tAluno*));
-                if (aprovados == NULL) {
-                    printf("Erro na alocacao de memoria no vetor de aprovados!\n");
-                    DesalocaMemoria(alunos, a);
-                    exit(1);
-                }
-                p++;
-            }else {
-                aprovados = (tAluno**)realloc(aprovados, (p +1) * sizeof(tAluno*));
-                if (aprovados == NULL) {
-                    printf("Erro na realocacao de memoria no vetor de aprovados!\n");
-                    DesalocaMemoria(aprovados, p);
-                    DesalocaMemoria(alunos, a);
-                    exit(1);
-                }
-            }
             aprovados[p] = alunos[a];
             p++;
         }
     }
-
+    aprovados = (tAluno **)realloc(aprovados, p * (sizeof(tAluno*)));
     OrdenaAprovados(aprovados, p);
 
     for (i = 0; i < p; i++) {
@@ -70,6 +68,8 @@ int main () {
     printf("\n");
 
     DesalocaMemoria(alunos, qtdAlunos);
+
+    free(alunos);
     free(aprovados);
 
     return 0;
