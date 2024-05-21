3a3946997e3e535595de6189c3f416cd20cce90b
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-05-21 10:40:48 -0300

    Adicionando arquivo main

---
+++ 07_TAD_opaco/TAD_opac_03/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "locadora.h"
+
+#define TAM_STRING_OPERACAO 10
+#define NUM_OPCOES 4
+
+int main() {
+    tLocadora *locadora;
+    char parada = '\0';
+    char operacao[TAM_STRING_OPERACAO];
+    char opcoes[NUM_OPCOES][TAM_STRING_OPERACAO] = {"Cadastrar", "Alugar", "Devolver", "Estoque"};
+
+    locadora = CriaLocadora();
+
+    while(scanf("%s", operacao) == 1) {
+        if (strcmp(operacao, opcoes[0]) == 0) {
+            LerCadastroLocadora(locadora);
+            printf("\n");
+
+            if ((scanf("%c", &parada) == 1) && (parada == '#')) {
+                continue;
+            }
+        }
+        if (strcmp(operacao, opcoes[1]) == 0) {
+            LerAluguelLocadora(locadora);
+            printf("\n");
+
+            if ((scanf("%c", &parada) == 1) && (parada == '#')) {
+                continue;
+            }
+        }
+        if (strcmp(operacao, opcoes[2]) == 0) {
+            LerDevolucaoLocadora(locadora);
+            printf("\n");
+
+            if ((scanf("%c", &parada) == 1) && (parada == '#')) {
+                continue;
+            }
+        }
+        if (strcmp(operacao, opcoes[3]) == 0) {
+            OrdenarFilmesLocadora(locadora);
+            ConsultarEstoqueLocadora(locadora);
+
+            if ((scanf("%c", &parada) == 1) && (parada == '#')) {
+                continue;
+            }
+        }
+    }
+    ConsultarLucroLocadora(locadora);
+
+    DestruirLocadora(locadora);
+
+    return 0;
+}
