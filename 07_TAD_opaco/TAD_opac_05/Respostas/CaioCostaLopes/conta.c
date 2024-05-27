f4283c7adf057c05bee6180ad4b4f047ef0615e9
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-05-27 13:03:37 -0300

    Adicionando arquivo .c da biblioteca da conta

---
+++ 07_TAD_opaco/TAD_opac_05/Respostas/CaioCostaLopes/conta.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "conta.h"
+#include "usuario.h"
+
+struct Conta {
+    int numero;
+    float saldo;
+    tUsuario *user;
+};
+
+tConta *CriaConta() {
+    tConta *conta = (tConta*)malloc(sizeof(tConta));
+    if (conta == NULL) {
+        printf("Erro na alocacao de memoria da conta ");
+        return conta;
+    }
+
+    conta->user = CriaUsuario();
+    if ((*conta).user == NULL) {
+        DestroiConta(conta);
+        return conta;
+    }
+
+    conta->numero = -1;
+    conta->saldo = 0;
+
+    return conta;
+}
+
+void LeConta(tConta *conta) {
+    LeUsuario((*conta).user);
+    scanf("%d\n", &conta->numero);
+}
+
+int VerificaConta(tConta *conta, int numero) {
+    return ((*conta).saldo == numero);
+}
+
+void SaqueConta(tConta *conta, int valor) {
+    if ((*conta).saldo >= valor) {
+        conta->saldo -= valor;
+    }
+}
+
+void DepositoConta(tConta *conta, float valor) {
+    conta->saldo += valor;
+}
+
+void TransferenciaConta(tConta *destino, tConta *origem, float valor) {
+    if ((*origem).saldo >= valor) {
+        SaqueConta(origem, valor);
+        DepositoConta(destino, valor);
+    }
+}
+
+void ImprimeConta(tConta *conta) {
+    printf("Conta: %d\n", (*conta).numero);
+    printf("Saldo: R$ %.2f\n", (*conta).saldo);
+    ImprimeUsuario((*conta).user);
+}
+
+void DestroiConta(tConta *conta) {
+    if (conta != NULL) {
+        if ((*conta).user != NULL) {
+            DestroiUsuario((*conta).user);
+        }
+        free(conta);
+        conta = NULL;
+    }
+}
