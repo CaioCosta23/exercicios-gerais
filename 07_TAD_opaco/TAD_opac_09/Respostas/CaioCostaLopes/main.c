1351da2e3fc697bf1e319f61637d536edf3f821b
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-05-28 18:15:10 -0300

    Adicionando arquivo main

---
+++ 07_TAD_opaco/TAD_opac_09/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "empresa.h"
+
+void EncerraPrograma(tEmpresa *empresas[], int qtdAlocacoes) {
+    int a;
+
+    for (a = 0; a < qtdAlocacoes; a++) {
+        ApagaEmpresa(empresas[a]);
+    }
+}
+
+void VerificaAlocacao(tEmpresa *empresas[], int posicaoAnalisada) {
+    if (empresas[posicaoAnalisada] == NULL) {
+        printf("%d", (posicaoAnalisada + 1));
+        EncerraPrograma(empresas, (posicaoAnalisada + 1));
+        exit(1);
+    }
+}
+
+
+int main() {
+    int qtdEmpresas, e;
+
+    scanf("%d", &qtdEmpresas);
+
+    tEmpresa *empresas[qtdEmpresas];
+
+    for (e = 0; e < qtdEmpresas; e++) {
+        empresas[e] = CriaEmpresas();
+        VerificaAlocacao(empresas, (e));
+        LeEmpresa(empresas[e]);
+        VerificaAlocacao(empresas, (e));
+    }
+    for (e = 0; e < qtdEmpresas; e++) {
+        ImprimeEmpresa(empresas[e]);
+    }
+    
+    EncerraPrograma(empresas, qtdEmpresas);
+}
