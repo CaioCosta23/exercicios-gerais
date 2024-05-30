e1964d425083312923540e509d9d22709806de58
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-05-30 20:22:59 -0300

    Adicionando arquivo .c da biblioteca da lesao

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_13/Respostas/CaioCostaLopes/lesao.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lesao.h"
+
+tLesao *CriaLesao() {
+    tLesao *lesao = (tLesao*)malloc(sizeof(tLesao));
+
+    if (lesao == NULL) {
+        printf("Erro na alocacao de memoria da lesao!\n");
+        return lesao;
+    }
+
+    lesao->id = (char*)malloc(TAM_ID * sizeof(char));
+    if ((*lesao).id == NULL) {
+        printf("Erro na alocacao de memoria do ID da lesao!\n");
+        LiberaLesao(lesao);
+    }
+
+    lesao->diagnostico = (char*)malloc(TAM_DIAG * sizeof(char));
+    if ((*lesao).diagnostico == NULL) {
+        printf("Erro na alocacao de memoria no nome do diagnostico da lesao!\n");
+        LiberaLesao(lesao);
+    }
+
+    lesao->regiao_corpo = (char*)malloc(TAM_REG * sizeof(char));
+    if ((*lesao).regiao_corpo == NULL) {
+        printf("Erro na alocacao de memoria do nome da regiao do corpo onde foi constatada a lesao");
+        LiberaLesao(lesao);
+    }
+
+    lesao->chances_malignidade = 0;
+
+    return lesao;
+}
+
+void LeLesao(tLesao *l) {
+    scanf("%s", l->id);
+    scanf("%s", l->diagnostico);
+    scanf("%s", l->regiao_corpo);
+    scanf("%d\n", l->chances_malignidade);
+}
+
+char *GetIdLesao(tLesao *l) {
+    return (*l).id;
+}
+
+int PrecisaCirurgiaLesao(tLesao *l) {
+    return ((*l).chances_malignidade > 50);
+}
+
+void LiberaLesao(tLesao *l) {
+    if (l != NULL) {
+        if ((*l).id != NULL) {
+            if ((*l).diagnostico != NULL) {
+                if ((*l).regiao_corpo != NULL) {
+                    free((*l).regiao_corpo);
+                    l->regiao_corpo = NULL;
+                }
+                free((*l).diagnostico);
+                l->diagnostico = NULL;
+            }
+            free((*l).id);
+            l->id = NULL;
+        }
+        free(l);
+        l = NULL;
+    }
+}
