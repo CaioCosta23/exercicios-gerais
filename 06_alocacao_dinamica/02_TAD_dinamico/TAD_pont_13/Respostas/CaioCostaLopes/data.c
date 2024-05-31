74846d858af385ae477ca28033f0fa7ca4e1230a
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-05-30 21:41:21 -0300

    Refazendo função de calcular idade de um paciente

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_13/Respostas/CaioCostaLopes/data.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_13/Respostas/CaioCostaLopes/data.c
@@ -28,61 +28,8 @@ tData *LeData() {
     return data;
 }
 
-int CalCulaIdadeData(tData *nascimento, tData *diacalc) {
-    int dia, mes, ano, d, m, a;
-    int diaFinalMes;
-    int bissexto;
-    int idade = 0;
-
-    dia = (*nascimento).dia;
-    mes = (*nascimento).mes;
-    ano = (*nascimento).ano;
-
-    for (a = ano; a <= (*diacalc).ano; a++) {
-        if (((a % 4 == 0) && (a % 100 != 0)) || ((a % 100 == 0) && (a % 400 == 0))) {
-            bissexto = 1;
-        }else {
-            bissexto = 0;
-        }
-        if (a == ano) {
-            m = mes;
-        }else {
-            m = 1;
-        }
-        while (m <= 12) {
-            if ((m == 1) || (m == 3) || (m == 5) || (m == 7) || (m == 8) || (m == 10) || (m == 12)) {
-                diaFinalMes = 31;
-            }else {
-                if ((m == 4) || (m = 6) || (m == 9) || (m == 11)) {
-                    diaFinalMes = 30;
-                }else {
-                    if (m == 2) {
-                        if (bissexto) {
-                            diaFinalMes = 29; 
-                        }else {
-                            diaFinalMes = 28;
-                        }
-                    }
-                }
-            }
-            if ((a = ano) && (m = mes)) {
-                d = dia;
-            }else {
-                d = 1;
-            }
-            while (d <= diaFinalMes) {
-                if ((d == (*nascimento).dia) && (m == (*nascimento).mes)) {
-                    idade += 1;
-                }
-                if ((d = (*diacalc).dia) && (m == (*diacalc).mes) && (a == (*diacalc).ano)) {
-                    return idade;
-                }
-                d++;
-            }
-            m++;
-        }
-    }
-    return idade;
+int CalculaIdadeData(tData *nascimento, tData *diacalc) {
+    return ((*diacalc).ano - (*nascimento).ano);
 }
 
 void ImprimeData(tData *d) {
