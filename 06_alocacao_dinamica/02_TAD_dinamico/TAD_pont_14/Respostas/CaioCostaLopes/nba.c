8597f1334d587e242a86a38669c32bb6c1346612
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-06-08 18:27:22 -0300

    Corrigindo condicionais de adição de franquias e partidas e formato de cálculo das porcentagens de estatística das conferências

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_14/Respostas/CaioCostaLopes/nba.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_14/Respostas/CaioCostaLopes/nba.c
@@ -46,41 +46,41 @@ tNBA RodaNBA(tNBA nba) {
         if (opcao == CAD_FRANQUIA) {
             if ((*nba).qtdfranquias == (*nba).maxfranquias) {
                 nba->listafranquias = (tFranquia*)realloc((*nba).listafranquias, ((*nba).maxfranquias + MAX_PT) * sizeof(tFranquia));
+                nba->maxfranquias += MAX_FR;
                 if ((*nba).listafranquias == NULL) {
                     printf("Erro na realocacao do vetor/lista de franquias da NBA!\n");
                     LiberaNBA(nba);
                     exit(1);
                 }
+            }
+            franquia = LeFranquia();
+            if (franquia == NULL) {
+                printf("%d da NBA!\n", ((*nba).qtdfranquias + 1));
+                LiberaNBA(nba);
+                exit(1);
             }else {
-                franquia = LeFranquia();
-                if (franquia == NULL) {
-                    printf("%d da NBA!\n", ((*nba).qtdfranquias + 1));
-                    LiberaNBA(nba);
-                    exit(1);
-                }else {
-                    nba->listafranquias[(*nba).qtdfranquias] = franquia;
-                    nba->qtdfranquias += 1;
-                }
+                nba->listafranquias[(*nba).qtdfranquias] = franquia;
+                nba->qtdfranquias += 1;
             }
         }
         if (opcao == CAD_PARTIDA) {
             if ((*nba).qtdpartidas == (*nba).maxpartidas) {
                 nba->listapartidas = (tPartida*)realloc((*nba).listapartidas, ((*nba).maxpartidas + MAX_PT) * sizeof(tPartida));
+                nba->maxpartidas += MAX_PT;
                 if ((*nba).listapartidas == NULL) {
                     printf("Erro na realocacao do vetor/lista de partidas da NBA!\n");
                     LiberaNBA(nba);
                     exit(1);
                 }
+            }
+            partida = LePartida();
+            if (partida == NULL) {
+                printf("%d da NBA!\n", ((*nba).qtdpartidas + 1));
+                LiberaNBA(nba);
+                exit(1);
             }else {
-                partida = LePartida();
-                if (partida == NULL) {
-                    printf("%d da NBA!\n", ((*nba).qtdpartidas + 1));
-                    LiberaNBA(nba);
-                    exit(1);
-                }else {
-                    nba->listapartidas[(*nba).qtdpartidas] = partida;
-                    nba->qtdpartidas += 1;
-                }
+                nba->listapartidas[(*nba).qtdpartidas] = partida;
+                nba->qtdpartidas += 1;
             }
         }
         if (opcao == ENCERRAR) {
@@ -126,7 +126,7 @@ tNBA RodaNBA(tNBA nba) {
 void ImprimeRelatorioNBA(tNBA nba) {
     int f;
     int vitoriasOeste = 0, derrotasOeste = 0, vitoriasLeste = 0, derrotasLeste = 0;
-    float porcentagemLeste = 0, porcentagemOeste = 0;
+    double porcentagemLeste = 0, porcentagemOeste = 0;
 
     for (f = 0; f < (*nba).qtdfranquias; f++) {
         ImprimeFranquia((*nba).listafranquias[f]);
@@ -141,10 +141,10 @@ void ImprimeRelatorioNBA(tNBA nba) {
         }
     }
     if (vitoriasLeste > 0) {
-        porcentagemLeste = (vitoriasLeste * 100) / (vitoriasLeste + derrotasLeste);
+        porcentagemLeste = (double)(vitoriasLeste * 100.00) / (double)(vitoriasLeste + derrotasLeste);
     }
     if (vitoriasOeste > 0) {
-        porcentagemOeste = (vitoriasOeste * 100) / (vitoriasOeste + derrotasOeste);
+        porcentagemOeste = (double)(vitoriasOeste * 100.00) / (double)(vitoriasOeste + derrotasOeste);
     }
 
     printf("LESTE %d %d %.2f\n", vitoriasLeste, derrotasLeste, porcentagemLeste);
