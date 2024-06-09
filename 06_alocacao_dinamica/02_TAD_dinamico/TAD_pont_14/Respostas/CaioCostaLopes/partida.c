01c48cb820fc0f78531c000bfbddae39ba46703c
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-06-09 14:54:00 -0300

    Adicionando arquivo .c da biblioteca da partida

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_14/Respostas/CaioCostaLopes/partida.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "partida.h"
+#include "franquia.h"
+
+tPartida LePartida() {
+    tPartida partida = (tPartida)malloc(sizeof(struct partida));
+
+    if (partida == NULL) {
+        printf("Erro na alocacao de memoria da partida ");
+        return partida;
+    }
+
+    partida->time1 = (char*)malloc(MAX_NOME * sizeof(char));
+    if ((*partida).time1 == NULL) {
+        printf("Erro na alocacao de memoria do time 1 da partida ");
+        LiberaPartida(partida);
+        return partida;
+    }
+    partida->time2 = (char*)malloc(MAX_NOME * sizeof(char));
+    if ((*partida).time2 == NULL) {
+        printf("Erro na alocacao de memoria do time 2 da partida ");
+        LiberaPartida(partida);
+        return partida;
+    }
+
+    partida->pontos1 = 0;
+    partida->pontos2 = 0;
+
+    scanf("%s @ %s %d %d\n", partida->time1, partida->time2, &partida->pontos1, &partida->pontos2);
+
+    return partida;
+}
+
+char *GetTime1Partida(tPartida p) {
+    return (*p).time1;
+}
+
+char *GetTime2Partida(tPartida p) {
+    return (*p).time2;
+}
+
+int GetVencedorPartida(tPartida p) {
+    if ((*p).pontos1 > (*p).pontos2) {
+        return TIME_CASA;
+    }else {
+        return TIME_FORA;
+    }
+}
+
+void ImprimePartida(tPartida p) {
+    // Função inutilizada (devido ao formato do exercício e do programa);
+}
+
+void LiberaPartida(tPartida p) {
+    if (p != NULL) {
+        if ((*p).time1 != NULL) {
+            if ((*p).time2 != NULL) {
+                free((*p).time2);
+                p->time2 = NULL;
+            }
+            free((*p).time1);
+            p->time1 = NULL;
+        }
+        free(p);
+        p = NULL;
+    }
+}
