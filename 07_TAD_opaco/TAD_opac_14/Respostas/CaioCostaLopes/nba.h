817650ff2cbd600025122dca6c15447767f86db5
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-07-08 11:38:41 -0300

    Adicionando arquivo .h da biblioteca da NBA

---
+++ 07_TAD_opaco/TAD_opac_14/Respostas/CaioCostaLopes/nba.h
@@ -0,0 +1,41 @@
+#ifndef _nba
+#define _nba
+
+#define MAX_FR 10 // Número máximo de franquias;
+#define MAX_PT 10 // Número máximo de partidas;
+#define CAD_FRANQUIA 'F' // Caractere de cadastro da franquia;
+#define CAD_PARTIDA 'P' // Caracttere de cadastro da partida;
+#define ENCERRAR 'E' // Caractere para encerra a NBA;
+
+typedef struct nba *tNBA;
+
+/**
+ * @brief Cria (aloca a memória alocada dinâmicamente da) NBA (com seus atributos inicializados);
+ * 
+ * @return tNBA Estrutura (do tipo ponteiro) do tipo "tNBA" que contém os dados da NBA;
+ */
+tNBA CriaNBA();
+
+/**
+ * @brief Roda a NBA permitindo a ainteração com o usuário;
+ * 
+ * @param nba Estrutura (do tipo ponteiro) do tipo "tNBA" que contém os dados da NBA;
+ * @return tNBA Estrutura (do tipo ponteiro) do tipo "tNBA" com os dados atualizado após a realização da NBA;
+ */
+tNBA RodaNBA(tNBA nba);
+
+/**
+ * @brief Imprime o relatório final da NBA;
+ * 
+ * @param nba Estrutura (do tipo ponteiro) do tipo "tNBA" que contém os dados da NBA que serão impressos;
+ */
+void ImprimeRelatorioNBA(tNBA nba);
+
+/**
+ * @brief Libera (a memória alocada dinamicamente da) NBA;
+ * 
+ * @param nba Estrutura (do tipo ponteiro) do tipo "tNBA" com os dados da NBA (e que serão liberados);
+ */
+void LiberaNBA(tNBA nba);
+
+#endif
