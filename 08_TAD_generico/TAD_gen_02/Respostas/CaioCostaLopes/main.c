865597b8ea098b6373b33b4a6d56dbae5991b6ce
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-08-23 11:18:58 -0300

    Adicionando arquivo main

---
+++ 08_TAD_generico/TAD_gen_02/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "gerenciadorpacotes.h"
+
+int main() {
+    int opcao, tipoElementos, numeroElementos, indiceListaPacotes;
+    tGerenciador *gerenciador;
+    tPacote *pacote;
+    bool programaEncerrado = false;
+
+    printf("tad_gen_02\n\n");
+
+    gerenciador = CriaGerenciador();
+
+    while (1) {
+        printf("Escolha uma opcao:\n");
+        printf("(1) Cadastrar um novo pacote\n");
+        printf("(2) Imprimir um pacote especifico\n");
+        printf("(3) Imprimir todos os pacotes e sair\n");
+
+        scanf("%d\n", &opcao);
+        
+        switch(opcao) {
+            case 1:
+                scanf("%d %d", &tipoElementos, &numeroElementos);
+                pacote = CriaPacote(tipoElementos, numeroElementos);
+                LePacote(pacote);
+                AdicionaPacoteNoGerenciador(gerenciador, pacote);
+                break;
+            case 2:
+                scanf("%d", &indiceListaPacotes);
+                ImprimePacoteNoIndice(gerenciador, indiceListaPacotes);
+                break;
+            case 3:
+                ImprimeTodosPacotes(gerenciador);
+                programaEncerrado = true;
+                break;
+            default:
+                printf("Opcao inexistente! Digite apenas 1, 2 ou 3!\n");
+                break;
+        }
+        if (programaEncerrado == true) {
+            break;
+        }
+    }
+
+    DestroiGerenciador(gerenciador);
+
+    return 0;
+}
