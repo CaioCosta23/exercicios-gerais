9164de99bdc6e621bdaab6ca92eedde9f50c8c15
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-08-23 10:26:46 -0300

    Adicionando arquivo main

---
+++ 08_TAD_generico/TAD_gen_01/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tadgen.h"
+
+int main() {
+    int tipo, numeroElementos;
+    tGeneric *tadGenerico;
+
+    printf("tad_gen_01\n");
+    printf("Digite o tipo e o numero de elementos:\n");
+
+    scanf("%d %d", &tipo, &numeroElementos);
+
+    tadGenerico = CriaGenerico(tipo, numeroElementos);
+    LeGenerico(tadGenerico);
+    ImprimeGenerico(tadGenerico);
+    DestroiGenerico(tadGenerico);
+
+    return 0;
+}
