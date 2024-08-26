cce2f2120d0d1623eb7388f08413d948f20d327b
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-08-26 11:07:54 -0300

    Adicionando arquivo .h da biblioteca do relatório

---
+++ 08_TAD_generico/TAD_gen_03/Respostas/CaioCostaLopes/relatorio.h
@@ -0,0 +1,13 @@
+#if !defined(_RELATORIO_H_)
+#define _RELATORO_H_
+
+#include "vector.h"
+
+/**
+ * @brief Imprime o relatório da lista de alunos;
+ * 
+ * @param alunos Ponteiro para a estrutura do tipo "Vector" que contém a lista de alunos os seus dados atualizados;
+ */
+void ImprimeRelatorio(Vector *alunos);
+
+#endif
