3b05afb36202f47e686ce0690410e14b8517ae01
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-08-26 18:48:01 -0300

    Adicionando arquivo .h da biblioteca da agencia

---
+++ 08_TAD_generico/TAD_gen_04/Respostas/CaioCostaLopes/agencia.h
@@ -0,0 +1,62 @@
+#if !defined(_AGENCIA_H_)
+#define _AGENCIA_H_
+
+#include "conta.h"
+#include "vector.h"
+
+typedef struct Agencia tAgencia;
+
+/**
+ * @brief Cria (aloca a mmemória dinâmicamente de) uma agência;
+ * 
+ * @return tAgencia* Ponteiro para a estrutura do tipo "tAgencia" que contém os campos de dados da agência inicializados;
+ */
+tAgencia *CriaAgencia();
+
+/**
+ * @brief Lê as informações de uma agência;
+ * 
+ * @param agencia Ponteiro para a estrutura o tipo "tAgencia" onde serão armazenadas as informações da agência;
+ */
+void LeAgencia(tAgencia *agencia);
+
+/**
+ * @brief Adicionauma conta na agência;
+ * 
+ * @param agencia Ponteiro para a estrutura do tipo "tAgencia" que contém os dados atualizados da agência;
+ * @param conta Ponteiro para a estrutura do tipo "tConta" que será adicionada a agência;
+ */
+void AdicionaConta(tAgencia *agencia, tConta *conta);
+
+/**
+ * @brief Comparaum número específico com o número de uma agência bancária;
+ * 
+ * @param numAgencia Número da agencia bancária;
+ * @param agencia2 Ponteiro para a estrutura do tipo "tAgencia" que contém os dados atualizados da agência a ser verificada;
+ * @return int 1 se os números forem iguais, 0 caso contrário;
+ */
+int CompararAgencia(int numAgencia, tAgencia *agencia2);
+
+/**
+ * @brief Obtém o saldo médio (média do saldo de todas as contas) da agência;
+ * 
+ * @param agencia Ponteiro para a estrutura do tipo "tAgencia" que contém os dados atualizados da agência;
+ * @return float Valor médio dos saldos das contas da agência;
+ */
+float GetSaldoMedioAgencia(tAgencia *agencia);
+
+/**
+ * @brief Função que imprime todos os dados de todas as contas da agência;
+ * 
+ * @param agencia Ponteiro para a estrutura do tipo "tAgencia" que contém os dados atualizados da agência que será impressa;
+ */
+void ImprimeAgencia(tAgencia *agencia);
+
+/**
+ * @brief Destrói (desaloca a memória dinamicamente de) uma agência;
+ * 
+ * @param agencia Ponteiro para a estrutura do tipo "tAgencia" que contém os dados atualizados da agência que será destruída;
+ */
+void DestroiAgencia(tAgencia *agencia);
+
+#endif
