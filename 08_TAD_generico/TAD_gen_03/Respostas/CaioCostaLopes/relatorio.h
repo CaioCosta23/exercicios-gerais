4d3001e385079e1f179fb8e612376011b1ba6f2a
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-08-26 11:34:20 -0300

    Adicionando observação de no comentério de documentação da função de imprimir relatório

--- 08_TAD_generico/TAD_gen_03/Respostas/CaioCostaLopes/relatorio.h
+++ 08_TAD_generico/TAD_gen_03/Respostas/CaioCostaLopes/relatorio.h
@@ -7,6 +7,7 @@
  * @brief Imprime o relatório da lista de alunos;
  * 
  * @param alunos Ponteiro para a estrutura do tipo "Vector" que contém a lista de alunos os seus dados atualizados;
+ * OBS: O relatório deve possuir a média das notas, porcentagem de alunos aprovados e a porcentagem de alunos aprovados em cada gênero
  */
 void ImprimeRelatorio(Vector *alunos);
 
