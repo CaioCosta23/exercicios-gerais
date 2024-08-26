9275d1e9c6fe273d41bc2e1ee9fa0271c2d3e488
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-08-26 13:00:36 -0300

    Adicionando arquivo .c da biblioteca do relatorio

---
+++ 08_TAD_generico/TAD_gen_03/Respostas/CaioCostaLopes/relatorio.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "relatorio.h"
+
+#define MASCULINO 'M'
+#define FEMININO 'F'
+#define OUTROS 'O'
+
+#define APROVACAO 6
+
+void ImprimeRelatorio(Vector *alunos) {
+    int v;
+    float somaNotas = 0, aprovadosMasculinos = 0, aprovadosFemininos = 0, aprovadosOutros = 0;
+
+    for (v = 0; v < VectorSize(alunos); v++) {
+        somaNotas += GetNotaAluno(VectorGet(alunos, v));
+
+        if (GetNotaAluno(VectorGet(alunos, v)) >= APROVACAO) {
+            if (GetGeneroAluno(VectorGet(alunos, v)) == MASCULINO) {
+                aprovadosMasculinos += 1;
+            }else {
+                if (GetGeneroAluno(VectorGet(alunos, v)) == FEMININO) {
+                    aprovadosFemininos += 1;
+                }else {
+                    if (GetGeneroAluno(VectorGet(alunos, v)) == OUTROS) {
+                        aprovadosOutros += 1;
+                    }
+                }
+            }
+        }
+    }
+    printf("Media das notas: %.2f\n", (somaNotas / VectorSize(alunos)));
+    printf("Porcentagem de alunos aprovados: %.2f\n", (((aprovadosMasculinos + aprovadosFemininos + aprovadosOutros) / (float)VectorSize(alunos)) * 100));
+    printf("Porcentagem de cada genero:\n");
+    printf("Masculino: %.2f\n", ((aprovadosMasculinos / (float)VectorSize(alunos)) * 100));
+    printf("Feminino: %.2f\n", ((aprovadosFemininos / (float)VectorSize(alunos)) * 100));
+    printf("Outros: %.2f\n", ((aprovadosOutros / (float)VectorSize(alunos)) * 100));
+    
+}
