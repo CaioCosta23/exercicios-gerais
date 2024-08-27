93f50e8af7f6106f262da76652c2a3253a56e45f
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-08-27 11:39:49 -0300

    Inserindo código para a adição dos dados no banco

--- 08_TAD_generico/TAD_gen_04/Respostas/CaioCostaLopes/main.c
+++ 08_TAD_generico/TAD_gen_04/Respostas/CaioCostaLopes/main.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "banco.h"
-#include "vector.h"
+
+#define CADASTRAR_AGENCIA 'A'
+#define CADASTRAR_CONTA 'C'
+#define FINALIZAR 'F'
 
 int main() {
+    char acao;
     tBanco *banco;
+    tAgencia *agencia;
+    tConta *conta;
+    int numeroAgencia;
+    bool programaFinalizado = false;
 
-    banco = CriaBanco();
-    
+    banco = CriaBanco();    
     LeBanco(banco);
+
+    while (1) {
+        scanf("%c\n", &acao);
+
+        switch (acao) {
+            case CADASTRAR_AGENCIA:
+                agencia = CriaAgencia();
+                LeAgencia(agencia);
+                AdicionaAgencia(banco, agencia);
+                break;
+            case CADASTRAR_CONTA:
+                conta = CriaConta();
+                LeConta(conta);
+                scanf("%d;", &numeroAgencia);
+                InsereContaBanco(banco, numeroAgencia, conta);
+                break;
+            case FINALIZAR:
+                programaFinalizado = true;
+                break;
+            default:
+                break;
+        }
+        if (programaFinalizado == true) {
+            break;
+        }
+    }
     ImprimeRelatorioBanco(banco);
     DestroiBanco(banco);
 
