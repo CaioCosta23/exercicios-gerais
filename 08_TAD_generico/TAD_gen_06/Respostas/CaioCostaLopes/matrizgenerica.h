7f8b32a49f20f437781d65e668b9f5b00a3996a3
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-09-01 23:05:56 -0300

    Adicionando arquivo .h da biblioteca da matriz genérica (arquivo correto e editado)

--- 08_TAD_generico/TAD_gen_06/Respostas/CaioCostaLopes/matrizgenerica.h
+++ 08_TAD_generico/TAD_gen_06/Respostas/CaioCostaLopes/matrizgenerica.h
@@ -1,112 +1,107 @@
 #ifndef _MATRIZ_GENERICA_H
 #define _MATRIZ_GENERICA_H
 
-// Adaptado do código compartilhado pelo prof. Thiago
-
 typedef struct matrizgenerica tMatrizGenerica;
 
 /**
- * @brief Cria uma estrutura genérica e dinâmica que pode armazenar uma Matriz de qualquer tipo de dados,
- * desde que esse tipo ocupe determinado número de bytes.
+ * @brief Cria (aloca a memória dinamicamente de) uma matriz genérica (que recebe qualquer tipo de dado);
  *
- * @param linhas - Numero de linhas da matriz
- * @param colunas - Numero de colunas da matriz
- * @param numByteElem - Número de Bytes que cada elemento da matriz ocupará
- *
- * @return Um ponteiro para a estrutura que armazena uma matriz genérica
+ * @param linhas Número de linhas da matriz;
+ * @param colunas Número de colunas da matriz;
+ * @param numByteElem Número de Bytes de cada elemento da matriz;
+ * OBS: Cria a matriz genérica desde que esse tipo ocupe determinado número de bytes;
+ * @return Ponteiro para a etrutura do tipo "tMatrizGenerica" que contém os campos de dados da matriz genérica inicializados;
  */
 tMatrizGenerica * CriaMatrizGenerica(int linhas, int colunas, int numByteElem);
 
 /**
- * @brief Libera a memória alocada dinamicamente para uma matriz
+ * @brief Obtém o número de linhas da matriz;
  *
- * @param mat - Ponteiro para a matriz a ser liberada
+ * @param mat Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica;
  *
- */
-void DestroiMatrizGenerica(tMatrizGenerica * mat);
-
-/**
- * @brief Obtém o número de linhas da matriz
- *
- * @param mat - Ponteiro para a matriz que queremos saber o número de linhas
- *
- * @return O número de linhas da matriz
+ * @return Número de linhas da matriz;
  */
 int ObtemNumeroLinhasMatrizGenerica(tMatrizGenerica * mat);
 
 /**
- * @brief Obtém o número de colunas da matriz
+ * @brief Obtém o número de colunas da matriz;
  *
- * @param mat - Ponteiro para a matriz que queremos saber o número de colunas
+ * @param mat Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica;
  *
- * @return O número de colunas da matriz
+ * @return Número de colunas da matriz;
  */
 int ObtemNumeroColunasMatrizGenerica(tMatrizGenerica * mat);
 
 /**
- * @brief Obtém ponteiro para um elemento qualquer da matriz genérica.
+ * @brief Obtém um elemento da matriz genérica;
  *
- * @param mat - Ponteiro para a matriz que queremos encontrar o elemento na linha e coluna desejada
- * @param linha - Linha que o elemento está
- * @param coluna - Coluna que o elemento está
+ * @param mat Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica;
+ * @param linha Linha em que o elemento está localizado na matriz geérica;
+ * @param coluna Coluna que o elemento está localizado na matriz genérica;
  *
- * @return Ponteiro para o elemento da matriz que está na linha e coluna passadas como parâmetro
+ * @return Ponteiro vazio para o elemento da matriz que está na linha e coluna passadas como parâmetro;
  */
 void * ObtemElementoMatrizGenerica(tMatrizGenerica * mat, int linha, int coluna);
 
 /**
- * @brief Atribui um elemento a uma posição de uma matriz.
+ * @brief Atribui um elemento a uma posição de uma matriz;
  *
- * @param mat - Ponteiro para a matriz que queremos atribuir o elemento na linha e coluna desejada
- * @param linha - Linha que o elemento ficará
- * @param coluna - Coluna que o elemento ficará
- * @param *elem - Ponteiro para o elemento que será colocado na matriz (será feita uma cópia)
- *
- * @return Ponteiro para o elemento da matriz que está na linha e coluna passadas como parâmetro
+ * @param mat Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica;
+ * @param linha Linha da matriz em que o elemento será atribuido;
+ * @param coluna Coluna da matriz em que o elemento será atribuido;
+ * @param elem Elemento que será adicionando a matriz;
  */
 void AtribuiElementoMatrizGenerica(tMatrizGenerica * mat, int linha, int coluna, void* elem);
 
 /**
- * @brief Imprime os elementos usando um callback para imprimir o tipo de dados da matriz
+ * @brief Imprime os elementos da matriz;
  *
- * @param mat - Ponteiro para a matriz que queremos atribuir o elemento na linha e coluna desejada
- * @param imprime_elemento - Ponteiro para a função (callback) que tem a capacidade de imprimir os dados de um elemento desta matriz
+ * @param mat Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica;
+ * @param imprime_elemento Ponteiro para a função de impressão de um elemento;
  */
 
 void ImprimirMatrizGenerica(tMatrizGenerica * mat, void (imprime_elemento(void*)));
 
 /**
- * @brief Encontra a transposta de uma matriz passada como parâmetro
+ * @brief Encontra a transposta de uma matriz;
  *
- * @param mat - A matriz que queremos encontrar a transposta
+ * @param mat Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica;
  *
- * @return Ponteiro para uma nova matriz, igual à transposta da matriz mat
+ * @return Ponteiro para a matriz do tipo "tMatrizGeérica" criada à partir da matriz transposta;
  */
 tMatrizGenerica *MatrizTransposta(tMatrizGenerica *mat);
 
 /**
- * @brief Calcula e retorna a multiplicação entre duas matrizes
+ * @brief Calcula a multiplicação entre duas matrizes;
  *
- * @param mat1 - A primeira matriz da multiplicação
- * @param mat2 - A segunda matriz da multiplicação
- * @param numByteTarget - Número de bytes do elemento resultante 
- * @param multi_elem - Ponteiro para função (callback) que realiza a multiplicação entre dois elementos do tipo de dados contido na matriz
- * @param soma_elem - Ponteiro para função (callback) que realiza a soma entre dois elementos do tipo de dados contido na matriz
+ * @param mat1 Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica 1;
+ * @param mat2 Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica 2;
+ * @param numByteTarget Número de bytes do elemento resultante da multiplicação entre os elementos de duas matrizes
+ * @param multi_elem Ponteiro para a função que multiplica elementos de duas matrizes elemento;
+ * @param soma_elem Ponteiro para a função que multiplica elementos de soma matrizes elemento;
  *
- * @return Ponteiro para uma nova matriz, igual a multiplicação entre mat1 e mat2.
+ * @return Ponteiro para a matriz do tipo "tMatrizGeérica" criada à partir da matriz resultante da multiplicação de duas outras matrizes genéricas;
  */
 tMatrizGenerica *MultiplicaMatrizes(tMatrizGenerica *mat1, tMatrizGenerica *mat2, int numByteTarget, void* (*multi_elem)(void*, void*), void* (*soma_elem)(void*,void*));
 
 
 /**
- * @brief Converte o tipo da matriz
+ * @brief Converte o tipo da matriz;
  *
- * @param mat - A matriz que queremos converter para o novo tipo
- * @param novoNumByteElem - Número de bytes do novo tipo da matriz
- * @param converte_elem - Ponteiro para função que tem a capacidade de converter o elemento do tipo original para o novo tipo
+ * @param mat Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica que terá seu tipo convertido;
+ * @param novoNumByteElem Número de bytes do novo tipo da matriz;
+ * @param converte_elem  Ponteiro para função que tem a capacidade de converter o elemento do tipo original para o novo tipo
  *
- * @return Ponteiro para uma nova matriz, no novo formato
+ * @return Ponteiro para a estrutura do tipo "tMatrizGenerica" que possui um novo tipo;
  */
 tMatrizGenerica *ConverteTipoMatriz(tMatrizGenerica *mat2, int novoNumByteElem, void* (*converte_elem)(void*));
 
+/**
+ * @brief Destrói (a memória alocada dinamicamente de) uma matriz;
+ *
+ * @param mat Ponteiro para a estrutura do tipo "tMatrizGenerica" que contém os dados atuailzados da matriz genérica;
+ */
+void DestroiMatrizGenerica(tMatrizGenerica * mat);
+
 #endif
+
