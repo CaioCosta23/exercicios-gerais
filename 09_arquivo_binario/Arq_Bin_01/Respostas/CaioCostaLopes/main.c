f280e4f5a00bb0ea6ab19ba2ec7ea0d488f5a2ab
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-09-06 11:15:43 -0300

    Adicionando arquivo main

---
+++ 09_arquivo_binario/Arq_Bin_01/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "aluno.h"
+
+int main() {
+    int numeroAlunos;
+    Aluno **alunos;
+
+    printf("Digite o numero de alunos: ");
+    scanf("%d\n", &numeroAlunos);
+
+    alunos = CriaVetorAlunos(numeroAlunos);
+    
+    LeAlunos(alunos, numeroAlunos);
+    SalvaAlunosBinario(alunos, "alunos.bin", numeroAlunos);
+
+    LiberaAlunos(alunos, numeroAlunos);
+
+    alunos = CriaVetorAlunos(numeroAlunos);
+    
+    CarregaAlunosBinario(alunos, "alunos.bin");
+    ImprimeAlunos(alunos, numeroAlunos);
+
+    LiberaAlunos(alunos, numeroAlunos);
+
+    return 0;
+}
