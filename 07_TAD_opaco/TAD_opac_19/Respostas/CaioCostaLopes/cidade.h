f207dc47301a5ee5e2acbf0329c921fb0966898f
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-10-29 10:43:52 -0300

    Corrrigindo comentários de ponteiros

--- 07_TAD_opaco/TAD_opac_19/Respostas/CaioCostaLopes/cidade.h
+++ 07_TAD_opaco/TAD_opac_19/Respostas/CaioCostaLopes/cidade.h
@@ -11,7 +11,7 @@
 #define DEFESA 'D' // Caractere que indica uma defesa;
 
 // A estrutura tCidade contém o nome da cidade, a quantidade de mísseis e defesas, e as listas de mísseis e defesas;
-typedef struct cidade * tCidade;
+typedef struct cidade *tCidade;
 
 /**
  * @brief Cria (aloca a memória dinamicamente de) uma cidade;
@@ -24,35 +24,35 @@ tCidade CriaCidade();
  * @brief Lê os dados de uma cidade;
  * 
  * @param cidade Ponteiro para a estrutura do tipo "tCidade" que contém os dados atualizados de uma cidade;
- * @return tCidade Ponteiro para a estrutura do tipo "tCidade" com os dados atualizados (e preenchidos) da cidade;
+ * @return tCidade Ponteiro para a estrutura (do tipo ponteiro) "tCidade" com os dados atualizados (e preenchidos) da cidade;
  */
 tCidade LeEntrada(tCidade cidade);
 
 /**
  * @brief Processa os dados dos ataques realizados na cidade;
  * 
- * @param cidade Ponteiro para a cidade que será atacada.
+ * @param cidade Ponteiro para a estrutura (do tipo ponteiro) "tCidade" que contém os dados atualizados de uma cidade;
  */
 void ProcessaAtaques(tCidade cidade);
 
 /**
  * @brief Imprime as informações de uma cidade;
  * 
- * @param cidade Ponteiro para a estrutura do tipo "tCidade" que contém os dados atualizados de uma cidade;
+ * @param cidade Ponteiro para a estrutura (do tipo ponteiro) "tCidade" que contém os dados atualizados de uma cidade;
  */
 void ImprimeCidade(tCidade cidade);
 
 /**
  * @brief Imprime os dados finais da cidade.
  * 
- * @param cidade Ponteiro para a estrutura do tipo "tCidade" que contém os dados atualizados de uma cidade;
+ * @param cidade Ponteiro para a estrutura (do tipo ponteiro) "tCidade" que contém os dados atualizados de uma cidade;
  */
 void ImprimeDados(tCidade cidade);
 
 /**
  * @brief Libera (desaloca a memória dinamicamente de) uma cidade;
  * 
- * @param cidade Ponteiro para a estrutura do tipo "tCidade" que contém os dados atualizados de uma cidade;
+ * @param cidade Ponteiro para a estrutura (do tipo ponteiro) "tCidade" que contém os dados atualizados de uma cidade;
  */
 void LiberaCidade(tCidade cidade);
 
