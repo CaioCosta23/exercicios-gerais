64ef80ff7eac9f4bb61f4c2f9a6989e9d92a84ff
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2024-10-29 11:12:07 -0300

    Adicionando arquivo main

---
+++ 07_TAD_opaco/TAD_opac_19/Respostas/CaioCostaLopes/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cidade.h"
+
+int main() {
+    tCidade cidade;
+
+    cidade = CriaCidade();
+
+    LeEntrada(cidade);
+    ProcessaAtaques(cidade);
+    ImprimeDados(cidade);
+
+    LiberaCidade(cidade);
+
+    return 0;
+}
